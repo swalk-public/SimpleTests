de1fb077d08d8248f0dc752090fe3e2f6c9e9226
xyzzy <xyzzy@xyzzys-iMac.local>
2025-05-28 23:58:06 -0500

    more bits

---
+++ bits/ui_bits_form3.c
@@ -0,0 +1,82 @@
+
+unsigned int ui_b00000001_form3_eq0(unsigned a) { return (a & 0x00000001) == 0x00000001; }
+unsigned int ui_b00000002_form3_eq0(unsigned a) { return (a & 0x00000002) == 0x00000002; }
+unsigned int ui_b00000004_form3_eq0(unsigned a) { return (a & 0x00000004) == 0x00000004; }
+unsigned int ui_b00000008_form3_eq0(unsigned a) { return (a & 0x00000008) == 0x00000008; }
+
+unsigned int ui_b00000010_form3_eq0(unsigned a) { return (a & 0x00000010) == 0x00000010; }
+unsigned int ui_b00000020_form3_eq0(unsigned a) { return (a & 0x00000020) == 0x00000020; }
+unsigned int ui_b00000040_form3_eq0(unsigned a) { return (a & 0x00000040) == 0x00000040; }
+unsigned int ui_b00000080_form3_eq0(unsigned a) { return (a & 0x00000080) == 0x00000080; }
+
+unsigned int ui_b00000100_form3_eq0(unsigned a) { return (a & 0x00000100) == 0x00000100; }
+unsigned int ui_b00000200_form3_eq0(unsigned a) { return (a & 0x00000200) == 0x00000200; }
+unsigned int ui_b00000400_form3_eq0(unsigned a) { return (a & 0x00000400) == 0x00000400; }
+unsigned int ui_b00000800_form3_eq0(unsigned a) { return (a & 0x00000800) == 0x00000800; }
+
+unsigned int ui_b00001000_form3_eq0(unsigned a) { return (a & 0x00001000) == 0x00001000; }
+unsigned int ui_b00002000_form3_eq0(unsigned a) { return (a & 0x00002000) == 0x00002000; }
+unsigned int ui_b00004000_form3_eq0(unsigned a) { return (a & 0x00004000) == 0x00004000; }
+unsigned int ui_b00008000_form3_eq0(unsigned a) { return (a & 0x00008000) == 0x00008000; }
+
+unsigned int ui_b00010000_form3_eq0(unsigned a) { return (a & 0x00010000) == 0x00010000; }
+unsigned int ui_b00020000_form3_eq0(unsigned a) { return (a & 0x00020000) == 0x00020000; }
+unsigned int ui_b00040000_form3_eq0(unsigned a) { return (a & 0x00040000) == 0x00040000; }
+unsigned int ui_b00080000_form3_eq0(unsigned a) { return (a & 0x00080000) == 0x00080000; }
+
+unsigned int ui_b00100000_form3_eq0(unsigned a) { return (a & 0x00100000) == 0x00100000; }
+unsigned int ui_b00200000_form3_eq0(unsigned a) { return (a & 0x00200000) == 0x00200000; }
+unsigned int ui_b00400000_form3_eq0(unsigned a) { return (a & 0x00400000) == 0x00400000; }
+unsigned int ui_b00800000_form3_eq0(unsigned a) { return (a & 0x00800000) == 0x00800000; }
+
+unsigned int ui_b01000000_form3_eq0(unsigned a) { return (a & 0x01000000) == 0x01000000; }
+unsigned int ui_b02000000_form3_eq0(unsigned a) { return (a & 0x02000000) == 0x02000000; }
+unsigned int ui_b04000000_form3_eq0(unsigned a) { return (a & 0x04000000) == 0x04000000; }
+unsigned int ui_b08000000_form3_eq0(unsigned a) { return (a & 0x08000000) == 0x08000000; }
+
+unsigned int ui_b10000000_form3_eq0(unsigned a) { return (a & 0x10000000) == 0x10000000; }
+unsigned int ui_b20000000_form3_eq0(unsigned a) { return (a & 0x20000000) == 0x20000000; }
+unsigned int ui_b40000000_form3_eq0(unsigned a) { return (a & 0x40000000) == 0x40000000; }
+unsigned int ui_b80000000_form3_eq0(unsigned a) { return (a & 0x80000000) == 0x80000000; }
+
+
+unsigned int ui_b00000001_form3_ne0(unsigned a) { return (a & 0x00000001) != 0x00000001; }
+unsigned int ui_b00000002_form3_ne0(unsigned a) { return (a & 0x00000002) != 0x00000002; }
+unsigned int ui_b00000004_form3_ne0(unsigned a) { return (a & 0x00000004) != 0x00000004; }
+unsigned int ui_b00000008_form3_ne0(unsigned a) { return (a & 0x00000008) != 0x00000008; }
+
+unsigned int ui_b00000010_form3_ne0(unsigned a) { return (a & 0x00000010) != 0x00000010; }
+unsigned int ui_b00000020_form3_ne0(unsigned a) { return (a & 0x00000020) != 0x00000020; }
+unsigned int ui_b00000040_form3_ne0(unsigned a) { return (a & 0x00000040) != 0x00000040; }
+unsigned int ui_b00000080_form3_ne0(unsigned a) { return (a & 0x00000080) != 0x00000080; }
+
+unsigned int ui_b00000100_form3_ne0(unsigned a) { return (a & 0x00000100) != 0x00000100; }
+unsigned int ui_b00000200_form3_ne0(unsigned a) { return (a & 0x00000200) != 0x00000200; }
+unsigned int ui_b00000400_form3_ne0(unsigned a) { return (a & 0x00000400) != 0x00000400; }
+unsigned int ui_b00000800_form3_ne0(unsigned a) { return (a & 0x00000800) != 0x00000800; }
+
+unsigned int ui_b00001000_form3_ne0(unsigned a) { return (a & 0x00001000) != 0x00001000; }
+unsigned int ui_b00002000_form3_ne0(unsigned a) { return (a & 0x00002000) != 0x00002000; }
+unsigned int ui_b00004000_form3_ne0(unsigned a) { return (a & 0x00004000) != 0x00004000; }
+unsigned int ui_b00008000_form3_ne0(unsigned a) { return (a & 0x00008000) != 0x00008000; }
+
+unsigned int ui_b00010000_form3_ne0(unsigned a) { return (a & 0x00010000) != 0x00010000; }
+unsigned int ui_b00020000_form3_ne0(unsigned a) { return (a & 0x00020000) != 0x00020000; }
+unsigned int ui_b00040000_form3_ne0(unsigned a) { return (a & 0x00040000) != 0x00040000; }
+unsigned int ui_b00080000_form3_ne0(unsigned a) { return (a & 0x00080000) != 0x00080000; }
+
+unsigned int ui_b00100000_form3_ne0(unsigned a) { return (a & 0x00100000) != 0x00100000; }
+unsigned int ui_b00200000_form3_ne0(unsigned a) { return (a & 0x00200000) != 0x00200000; }
+unsigned int ui_b00400000_form3_ne0(unsigned a) { return (a & 0x00400000) != 0x00400000; }
+unsigned int ui_b00800000_form3_ne0(unsigned a) { return (a & 0x00800000) != 0x00800000; }
+
+unsigned int ui_b01000000_form3_ne0(unsigned a) { return (a & 0x01000000) != 0x01000000; }
+unsigned int ui_b02000000_form3_ne0(unsigned a) { return (a & 0x02000000) != 0x02000000; }
+unsigned int ui_b04000000_form3_ne0(unsigned a) { return (a & 0x04000000) != 0x04000000; }
+unsigned int ui_b08000000_form3_ne0(unsigned a) { return (a & 0x08000000) != 0x08000000; }
+
+unsigned int ui_b10000000_form3_ne0(unsigned a) { return (a & 0x10000000) != 0x10000000; }
+unsigned int ui_b20000000_form3_ne0(unsigned a) { return (a & 0x20000000) != 0x20000000; }
+unsigned int ui_b40000000_form3_ne0(unsigned a) { return (a & 0x40000000) != 0x40000000; }
+unsigned int ui_b80000000_form3_ne0(unsigned a) { return (a & 0x80000000) != 0x80000000; }
+
