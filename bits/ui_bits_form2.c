de34bef2f965727bc2f45bf32089ea4507c71301
xyzzy <xyzzy@xyzzys-iMac.local>
2025-05-28 23:48:04 -0500

    more bits

---
+++ bits/ui_bits_form2.c
@@ -0,0 +1,84 @@
+
+unsigned int ui_b00000001_form2_eq0(unsigned a) { return ((a >> 0)  & 0x00000001) == 0x00000; }
+unsigned int ui_b00000002_form2_eq0(unsigned a) { return ((a >> 1)  & 0x00000001) == 0x00000; }
+unsigned int ui_b00000004_form2_eq0(unsigned a) { return ((a >> 2)  & 0x00000001) == 0x00000; }
+unsigned int ui_b00000008_form2_eq0(unsigned a) { return ((a >> 3)  & 0x00000001) == 0x00000; }
+
+unsigned int ui_b00000010_form2_eq0(unsigned a) { return ((a >> 4)  & 0x00000001) == 0x00000; }
+unsigned int ui_b00000020_form2_eq0(unsigned a) { return ((a >> 5)  & 0x00000001) == 0x00000; }
+unsigned int ui_b00000040_form2_eq0(unsigned a) { return ((a >> 6)  & 0x00000001) == 0x00000; }
+unsigned int ui_b00000080_form2_eq0(unsigned a) { return ((a >> 7)  & 0x00000001) == 0x00000; }
+
+unsigned int ui_b00000100_form2_eq0(unsigned a) { return ((a >> 8)  & 0x00000001) == 0x00000; }
+unsigned int ui_b00000200_form2_eq0(unsigned a) { return ((a >> 9)  & 0x00000001) == 0x00000; }
+unsigned int ui_b00000400_form2_eq0(unsigned a) { return ((a >> 10) & 0x00000001) == 0x00000; }
+unsigned int ui_b00000800_form2_eq0(unsigned a) { return ((a >> 11) & 0x00000001) == 0x00000; }
+
+unsigned int ui_b00001000_form2_eq0(unsigned a) { return ((a >> 12) & 0x00000001) == 0x00000; }
+unsigned int ui_b00002000_form2_eq0(unsigned a) { return ((a >> 13) & 0x00000001) == 0x00000; }
+unsigned int ui_b00004000_form2_eq0(unsigned a) { return ((a >> 14) & 0x00000001) == 0x00000; }
+unsigned int ui_b00008000_form2_eq0(unsigned a) { return ((a >> 15) & 0x00000001) == 0x00000; }
+
+unsigned int ui_b00010000_form2_eq0(unsigned a) { return ((a >> 16) & 0x00000001) == 0x00000; }
+unsigned int ui_b00020000_form2_eq0(unsigned a) { return ((a >> 17) & 0x00000001) == 0x00000; }
+unsigned int ui_b00040000_form2_eq0(unsigned a) { return ((a >> 18) & 0x00000001) == 0x00000; }
+unsigned int ui_b00080000_form2_eq0(unsigned a) { return ((a >> 19) & 0x00000001) == 0x00000; }
+
+unsigned int ui_b00100000_form2_eq0(unsigned a) { return ((a >> 20) & 0x00000001) == 0x00000; }
+unsigned int ui_b00200000_form2_eq0(unsigned a) { return ((a >> 21) & 0x00000001) == 0x00000; }
+unsigned int ui_b00400000_form2_eq0(unsigned a) { return ((a >> 22) & 0x00000001) == 0x00000; }
+unsigned int ui_b00800000_form2_eq0(unsigned a) { return ((a >> 23) & 0x00000001) == 0x00000; }
+
+unsigned int ui_b01000000_form2_eq0(unsigned a) { return ((a >> 24) & 0x00000001) == 0x00000; }
+unsigned int ui_b02000000_form2_eq0(unsigned a) { return ((a >> 25) & 0x00000001) == 0x00000; }
+unsigned int ui_b04000000_form2_eq0(unsigned a) { return ((a >> 26) & 0x00000001) == 0x00000; }
+unsigned int ui_b08000000_form2_eq0(unsigned a) { return ((a >> 27) & 0x00000001) == 0x00000; }
+
+unsigned int ui_b10000000_form2_eq0(unsigned a) { return ((a >> 28) & 0x00000001) == 0x00000; }
+unsigned int ui_b20000000_form2_eq0(unsigned a) { return ((a >> 29) & 0x00000001) == 0x00000; }
+unsigned int ui_b40000000_form2_eq0(unsigned a) { return ((a >> 30) & 0x00000001) == 0x00000; }
+unsigned int ui_b80000000_form2_eq0(unsigned a) { return ((a >> 31) & 0x00000001) == 0x00000; }
+
+
+
+unsigned int ui_b00000001_form2_ne0(unsigned a) { return ((a >> 0)  & 0x00000001) != 0x00000; }
+unsigned int ui_b00000002_form2_ne0(unsigned a) { return ((a >> 1)  & 0x00000001) != 0x00000; }
+unsigned int ui_b00000004_form2_ne0(unsigned a) { return ((a >> 2)  & 0x00000001) != 0x00000; }
+unsigned int ui_b00000008_form2_ne0(unsigned a) { return ((a >> 3)  & 0x00000001) != 0x00000; }
+
+unsigned int ui_b00000010_form2_ne0(unsigned a) { return ((a >> 4)  & 0x00000001) != 0x00000; }
+unsigned int ui_b00000020_form2_ne0(unsigned a) { return ((a >> 5)  & 0x00000001) != 0x00000; }
+unsigned int ui_b00000040_form2_ne0(unsigned a) { return ((a >> 6)  & 0x00000001) != 0x00000; }
+unsigned int ui_b00000080_form2_ne0(unsigned a) { return ((a >> 7)  & 0x00000001) != 0x00000; }
+
+unsigned int ui_b00000100_form2_ne0(unsigned a) { return ((a >> 8)  & 0x00000001) != 0x00000; }
+unsigned int ui_b00000200_form2_ne0(unsigned a) { return ((a >> 9)  & 0x00000001) != 0x00000; }
+unsigned int ui_b00000400_form2_ne0(unsigned a) { return ((a >> 10) & 0x00000001) != 0x00000; }
+unsigned int ui_b00000800_form2_ne0(unsigned a) { return ((a >> 11) & 0x00000001) != 0x00000; }
+
+unsigned int ui_b00001000_form2_ne0(unsigned a) { return ((a >> 12) & 0x00000001) != 0x00000; }
+unsigned int ui_b00002000_form2_ne0(unsigned a) { return ((a >> 13) & 0x00000001) != 0x00000; }
+unsigned int ui_b00004000_form2_ne0(unsigned a) { return ((a >> 14) & 0x00000001) != 0x00000; }
+unsigned int ui_b00008000_form2_ne0(unsigned a) { return ((a >> 15) & 0x00000001) != 0x00000; }
+
+unsigned int ui_b00010000_form2_ne0(unsigned a) { return ((a >> 16) & 0x00000001) != 0x00000; }
+unsigned int ui_b00020000_form2_ne0(unsigned a) { return ((a >> 17) & 0x00000001) != 0x00000; }
+unsigned int ui_b00040000_form2_ne0(unsigned a) { return ((a >> 18) & 0x00000001) != 0x00000; }
+unsigned int ui_b00080000_form2_ne0(unsigned a) { return ((a >> 19) & 0x00000001) != 0x00000; }
+
+unsigned int ui_b00100000_form2_ne0(unsigned a) { return ((a >> 20) & 0x00000001) != 0x00000; }
+unsigned int ui_b00200000_form2_ne0(unsigned a) { return ((a >> 21) & 0x00000001) != 0x00000; }
+unsigned int ui_b00400000_form2_ne0(unsigned a) { return ((a >> 22) & 0x00000001) != 0x00000; }
+unsigned int ui_b00800000_form2_ne0(unsigned a) { return ((a >> 23) & 0x00000001) != 0x00000; }
+
+unsigned int ui_b01000000_form2_ne0(unsigned a) { return ((a >> 24) & 0x00000001) != 0x00000; }
+unsigned int ui_b02000000_form2_ne0(unsigned a) { return ((a >> 25) & 0x00000001) != 0x00000; }
+unsigned int ui_b04000000_form2_ne0(unsigned a) { return ((a >> 26) & 0x00000001) != 0x00000; }
+unsigned int ui_b08000000_form2_ne0(unsigned a) { return ((a >> 27) & 0x00000001) != 0x00000; }
+
+unsigned int ui_b10000000_form2_ne0(unsigned a) { return ((a >> 28) & 0x00000001) != 0x00000; }
+unsigned int ui_b20000000_form2_ne0(unsigned a) { return ((a >> 29) & 0x00000001) != 0x00000; }
+unsigned int ui_b40000000_form2_ne0(unsigned a) { return ((a >> 30) & 0x00000001) != 0x00000; }
+unsigned int ui_b80000000_form2_ne0(unsigned a) { return ((a >> 31) & 0x00000001) != 0x00000; }
+
+
