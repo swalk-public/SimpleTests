f08f41168446d4b314331def8a209b7d324dc9de
xyzzy <xyzzy@xyzzys-iMac.local>
2025-07-13 20:41:42 -0500

    f_eq_jmp

---
+++ runnable/f_eq_jmp.c
@@ -0,0 +1,927 @@
+
+
+int exit_status;
+
+#include "my_assert.h"
+
+int eq_m131072d062500(float f) {
+  if (f == -131072.062500f)
+    return 1;
+  else
+    return 0;
+}
+int eq_m131072d031250(float f) {
+  if (f == -131072.031250f)
+    return 1;
+  else
+    return 0;
+}
+int eq_m131072d015625(float f) {
+  if (f == -131072.015625f)
+    return 1;
+  else
+    return 0;
+}
+
+int eq_m65536d062500(float f) {
+  if (f == -65536.062500f)
+    return 1;
+  else
+    return 0;
+}
+int eq_m65536d031250(float f) {
+  if (f == -65536.031250f)
+    return 1;
+  else
+    return 0;
+}
+int eq_m65536d015625(float f) {
+  if (f == -65536.015625f)
+    return 1;
+  else
+    return 0;
+}
+
+int eq_m32768d062500(float f) {
+  if (f == -32768.062500f)
+    return 1;
+  else
+    return 0;
+}
+int eq_m32768d031250(float f) {
+  if (f == -32768.031250f)
+    return 1;
+  else
+    return 0;
+}
+int eq_m32768d015625(float f) {
+  if (f == -32768.015625f)
+    return 1;
+  else
+    return 0;
+}
+
+int eq_m16384d062500(float f) {
+  if (f == -16384.062500f)
+    return 1;
+  else
+    return 0;
+}
+int eq_m16384d031250(float f) {
+  if (f == -16384.031250f)
+    return 1;
+  else
+    return 0;
+}
+int eq_m16384d015625(float f) {
+  if (f == -16384.015625f)
+    return 1;
+  else
+    return 0;
+}
+
+int eq_m8192d062500(float f) {
+  if (f == -8192.062500f)
+    return 1;
+  else
+    return 0;
+}
+int eq_m8192d031250(float f) {
+  if (f == -8192.031250f)
+    return 1;
+  else
+    return 0;
+}
+int eq_m8192d015625(float f) {
+  if (f == -8192.015625f)
+    return 1;
+  else
+    return 0;
+}
+
+int eq_m4096d062500(float f) {
+  if (f == -4096.062500f)
+    return 1;
+  else
+    return 0;
+}
+int eq_m4096d031250(float f) {
+  if (f == -4096.031250f)
+    return 1;
+  else
+    return 0;
+}
+int eq_m4096d015625(float f) {
+  if (f == -4096.015625f)
+    return 1;
+  else
+    return 0;
+}
+
+int eq_m2048d062500(float f) {
+  if (f == -2048.062500f)
+    return 1;
+  else
+    return 0;
+}
+int eq_m2048d031250(float f) {
+  if (f == -2048.031250f)
+    return 1;
+  else
+    return 0;
+}
+int eq_m2048d015625(float f) {
+  if (f == -2048.015625f)
+    return 1;
+  else
+    return 0;
+}
+
+int eq_m1024d062500(float f) {
+  if (f == -1024.062500f)
+    return 1;
+  else
+    return 0;
+}
+int eq_m1024d031250(float f) {
+  if (f == -1024.031250f)
+    return 1;
+  else
+    return 0;
+}
+int eq_m1024d015625(float f) {
+  if (f == -1024.015625f)
+    return 1;
+  else
+    return 0;
+}
+
+int eq_m512d062500(float f) {
+  if (f == -512.062500f)
+    return 1;
+  else
+    return 0;
+}
+int eq_m512d031250(float f) {
+  if (f == -512.031250f)
+    return 1;
+  else
+    return 0;
+}
+int eq_m512d015625(float f) {
+  if (f == -512.015625f)
+    return 1;
+  else
+    return 0;
+}
+
+int eq_m256d062500(float f) {
+  if (f == -256.062500f)
+    return 1;
+  else
+    return 0;
+}
+int eq_m256d031250(float f) {
+  if (f == -256.031250f)
+    return 1;
+  else
+    return 0;
+}
+int eq_m256d015625(float f) {
+  if (f == -256.015625f)
+    return 1;
+  else
+    return 0;
+}
+
+int eq_m128d062500(float f) {
+  if (f == -128.062500f)
+    return 1;
+  else
+    return 0;
+}
+int eq_m128d031250(float f) {
+  if (f == -128.031250f)
+    return 1;
+  else
+    return 0;
+}
+int eq_m128d015625(float f) {
+  if (f == -128.015625f)
+    return 1;
+  else
+    return 0;
+}
+
+int eq_m64d062500(float f) {
+  if (f == -64.062500f)
+    return 1;
+  else
+    return 0;
+}
+int eq_m64d031250(float f) {
+  if (f == -64.031250f)
+    return 1;
+  else
+    return 0;
+}
+int eq_m64d015625(float f) {
+  if (f == -64.015625f)
+    return 1;
+  else
+    return 0;
+}
+
+int eq_m32d062500(float f) {
+  if (f == -32.062500f)
+    return 1;
+  else
+    return 0;
+}
+int eq_m32d031250(float f) {
+  if (f == -32.031250f)
+    return 1;
+  else
+    return 0;
+}
+int eq_m32d015625(float f) {
+  if (f == -32.015625f)
+    return 1;
+  else
+    return 0;
+}
+
+int eq_m16d062500(float f) {
+  if (f == -16.062500f)
+    return 1;
+  else
+    return 0;
+}
+int eq_m16d031250(float f) {
+  if (f == -16.031250f)
+    return 1;
+  else
+    return 0;
+}
+int eq_m16d015625(float f) {
+  if (f == -16.015625f)
+    return 1;
+  else
+    return 0;
+}
+
+int eq_m8d062500(float f) {
+  if (f == -8.062500f)
+    return 1;
+  else
+    return 0;
+}
+int eq_m8d031250(float f) {
+  if (f == -8.031250f)
+    return 1;
+  else
+    return 0;
+}
+int eq_m8d015625(float f) {
+  if (f == -8.015625f)
+    return 1;
+  else
+    return 0;
+}
+
+int eq_m4d062500(float f) {
+  if (f == -4.062500f)
+    return 1;
+  else
+    return 0;
+}
+int eq_m4d031250(float f) {
+  if (f == -4.031250f)
+    return 1;
+  else
+    return 0;
+}
+int eq_m4d015625(float f) {
+  if (f == -4.015625f)
+    return 1;
+  else
+    return 0;
+}
+
+int eq_m2d062500(float f) {
+  if (f == -2.062500f)
+    return 1;
+  else
+    return 0;
+}
+int eq_m2d031250(float f) {
+  if (f == -2.031250f)
+    return 1;
+  else
+    return 0;
+}
+int eq_m2d015625(float f) {
+  if (f == -2.015625f)
+    return 1;
+  else
+    return 0;
+}
+
+int eq_m1d062500(float f) {
+  if (f == -1.062500f)
+    return 1;
+  else
+    return 0;
+}
+int eq_m1d031250(float f) {
+  if (f == -1.031250f)
+    return 1;
+  else
+    return 0;
+}
+int eq_m1d015625(float f) {
+  if (f == -1.015625f)
+    return 1;
+  else
+    return 0;
+}
+
+int eq_m0d062500(float f) {
+  if (f == -0.062500f)
+    return 1;
+  else
+    return 0;
+}
+int eq_m0d031250(float f) {
+  if (f == -0.031250f)
+    return 1;
+  else
+    return 0;
+}
+int eq_m0d015625(float f) {
+  if (f == -0.015625f)
+    return 1;
+  else
+    return 0;
+}
+
+//--------
+
+int eq_0d015625(float f) {
+  if (f == 0.015625f)
+    return 1;
+  else
+    return 0;
+}
+int eq_0d031250(float f) {
+  if (f == 0.031250f)
+    return 1;
+  else
+    return 0;
+}
+int eq_0d062500(float f) {
+  if (f == 0.062500f)
+    return 1;
+  else
+    return 0;
+}
+
+int eq_1d015625(float f) {
+  if (f == 1.015625f)
+    return 1;
+  else
+    return 0;
+}
+int eq_1d031250(float f) {
+  if (f == 1.031250f)
+    return 1;
+  else
+    return 0;
+}
+int eq_1d062500(float f) {
+  if (f == 1.062500f)
+    return 1;
+  else
+    return 0;
+}
+
+int eq_2d015625(float f) {
+  if (f == 2.015625f)
+    return 1;
+  else
+    return 0;
+}
+int eq_2d031250(float f) {
+  if (f == 2.031250f)
+    return 1;
+  else
+    return 0;
+}
+int eq_2d062500(float f) {
+  if (f == 2.062500f)
+    return 1;
+  else
+    return 0;
+}
+
+int eq_4d015625(float f) {
+  if (f == 4.015625f)
+    return 1;
+  else
+    return 0;
+}
+int eq_4d031250(float f) {
+  if (f == 4.031250f)
+    return 1;
+  else
+    return 0;
+}
+int eq_4d062500(float f) {
+  if (f == 4.062500f)
+    return 1;
+  else
+    return 0;
+}
+
+int eq_8d015625(float f) {
+  if (f == 8.015625f)
+    return 1;
+  else
+    return 0;
+}
+int eq_8d031250(float f) {
+  if (f == 8.031250f)
+    return 1;
+  else
+    return 0;
+}
+int eq_8d062500(float f) {
+  if (f == 8.062500f)
+    return 1;
+  else
+    return 0;
+}
+
+int eq_16d015625(float f) {
+  if (f == 16.015625f)
+    return 1;
+  else
+    return 0;
+}
+int eq_16d031250(float f) {
+  if (f == 16.031250f)
+    return 1;
+  else
+    return 0;
+}
+int eq_16d062500(float f) {
+  if (f == 16.062500f)
+    return 1;
+  else
+    return 0;
+}
+
+int eq_32d015625(float f) {
+  if (f == 32.015625f)
+    return 1;
+  else
+    return 0;
+}
+int eq_32d031250(float f) {
+  if (f == 32.031250f)
+    return 1;
+  else
+    return 0;
+}
+int eq_32d062500(float f) {
+  if (f == 32.062500f)
+    return 1;
+  else
+    return 0;
+}
+
+int eq_64d015625(float f) {
+  if (f == 64.015625f)
+    return 1;
+  else
+    return 0;
+}
+int eq_64d031250(float f) {
+  if (f == 64.031250f)
+    return 1;
+  else
+    return 0;
+}
+int eq_64d062500(float f) {
+  if (f == 64.062500f)
+    return 1;
+  else
+    return 0;
+}
+
+int eq_128d015625(float f) {
+  if (f == 128.015625f)
+    return 1;
+  else
+    return 0;
+}
+int eq_128d031250(float f) {
+  if (f == 128.031250f)
+    return 1;
+  else
+    return 0;
+}
+int eq_128d062500(float f) {
+  if (f == 128.062500f)
+    return 1;
+  else
+    return 0;
+}
+
+int eq_256d015625(float f) {
+  if (f == 256.015625f)
+    return 1;
+  else
+    return 0;
+}
+int eq_256d031250(float f) {
+  if (f == 256.031250f)
+    return 1;
+  else
+    return 0;
+}
+int eq_256d062500(float f) {
+  if (f == 256.062500f)
+    return 1;
+  else
+    return 0;
+}
+
+int eq_512d015625(float f) {
+  if (f == 512.015625f)
+    return 1;
+  else
+    return 0;
+}
+int eq_512d031250(float f) {
+  if (f == 512.031250f)
+    return 1;
+  else
+    return 0;
+}
+int eq_512d062500(float f) {
+  if (f == 512.062500f)
+    return 1;
+  else
+    return 0;
+}
+
+int eq_1024d015625(float f) {
+  if (f == 1024.015625f)
+    return 1;
+  else
+    return 0;
+}
+int eq_1024d031250(float f) {
+  if (f == 1024.031250f)
+    return 1;
+  else
+    return 0;
+}
+int eq_1024d062500(float f) {
+  if (f == 1024.062500f)
+    return 1;
+  else
+    return 0;
+}
+
+int eq_2048d015625(float f) {
+  if (f == 2048.015625f)
+    return 1;
+  else
+    return 0;
+}
+int eq_2048d031250(float f) {
+  if (f == 2048.031250f)
+    return 1;
+  else
+    return 0;
+}
+int eq_2048d062500(float f) {
+  if (f == 2048.062500f)
+    return 1;
+  else
+    return 0;
+}
+
+int eq_4096d015625(float f) {
+  if (f == 4096.015625f)
+    return 1;
+  else
+    return 0;
+}
+int eq_4096d031250(float f) {
+  if (f == 4096.031250f)
+    return 1;
+  else
+    return 0;
+}
+int eq_4096d062500(float f) {
+  if (f == 4096.062500f)
+    return 1;
+  else
+    return 0;
+}
+
+int eq_8192d015625(float f) {
+  if (f == 8192.015625f)
+    return 1;
+  else
+    return 0;
+}
+int eq_8192d031250(float f) {
+  if (f == 8192.031250f)
+    return 1;
+  else
+    return 0;
+}
+int eq_8192d062500(float f) {
+  if (f == 8192.062500f)
+    return 1;
+  else
+    return 0;
+}
+
+int eq_16384d015625(float f) {
+  if (f == 16384.015625f)
+    return 1;
+  else
+    return 0;
+}
+int eq_16384d031250(float f) {
+  if (f == 16384.031250f)
+    return 1;
+  else
+    return 0;
+}
+int eq_16384d062500(float f) {
+  if (f == 16384.062500f)
+    return 1;
+  else
+    return 0;
+}
+
+int eq_32768d015625(float f) {
+  if (f == 32768.015625f)
+    return 1;
+  else
+    return 0;
+}
+int eq_32768d031250(float f) {
+  if (f == 32768.031250f)
+    return 1;
+  else
+    return 0;
+}
+int eq_32768d062500(float f) {
+  if (f == 32768.062500f)
+    return 1;
+  else
+    return 0;
+}
+
+int eq_65536d015625(float f) {
+  if (f == 65536.015625f)
+    return 1;
+  else
+    return 0;
+}
+int eq_65536d031250(float f) {
+  if (f == 65536.031250f)
+    return 1;
+  else
+    return 0;
+}
+int eq_65536d062500(float f) {
+  if (f == 65536.062500f)
+    return 1;
+  else
+    return 0;
+}
+
+int eq_131072d015625(float f) {
+  if (f == 131072.015625f)
+    return 1;
+  else
+    return 0;
+}
+int eq_131072d031250(float f) {
+  if (f == 131072.031250f)
+    return 1;
+  else
+    return 0;
+}
+int eq_131072d062500(float f) {
+  if (f == 131072.062500f)
+    return 1;
+  else
+    return 0;
+}
+
+static float f;
+
+int main() {
+  f = -131072.031250f;
+  my_assert(eq_m131072d062500(f) != 1);
+  my_assert(eq_m131072d031250(f) == 1);
+  my_assert(eq_m131072d015625(f) != 1);
+
+  f = -65536.031250f;
+  my_assert(eq_m65536d062500(f) != 1);
+  my_assert(eq_m65536d031250(f) == 1);
+  my_assert(eq_m65536d015625(f) != 1);
+
+  f = -32768.031250f;
+  my_assert(eq_m32768d062500(f) != 1);
+  my_assert(eq_m32768d031250(f) == 1);
+  my_assert(eq_m32768d015625(f) != 1);
+
+  f = -16384.031250f;
+  my_assert(eq_m16384d062500(f) != 1);
+  my_assert(eq_m16384d031250(f) == 1);
+  my_assert(eq_m16384d015625(f) != 1);
+
+  f = -8192.031250f;
+  my_assert(eq_m8192d062500(f) != 1);
+  my_assert(eq_m8192d031250(f) == 1);
+  my_assert(eq_m8192d015625(f) != 1);
+
+  f = -4096.031250f;
+  my_assert(eq_m4096d062500(f) != 1);
+  my_assert(eq_m4096d031250(f) == 1);
+  my_assert(eq_m4096d015625(f) != 1);
+
+  f = -2048.031250f;
+  my_assert(eq_m2048d062500(f) != 1);
+  my_assert(eq_m2048d031250(f) == 1);
+  my_assert(eq_m2048d015625(f) != 1);
+
+  f = -1024.031250f;
+  my_assert(eq_m1024d062500(f) != 1);
+  my_assert(eq_m1024d031250(f) == 1);
+  my_assert(eq_m1024d015625(f) != 1);
+
+  f = -512.031250f;
+  my_assert(eq_m512d062500(f) != 1);
+  my_assert(eq_m512d031250(f) == 1);
+  my_assert(eq_m512d015625(f) != 1);
+
+  f = -256.031250f;
+  my_assert(eq_m256d062500(f) != 1);
+  my_assert(eq_m256d031250(f) == 1);
+  my_assert(eq_m256d015625(f) != 1);
+
+  f = -128.031250f;
+  my_assert(eq_m128d062500(f) != 1);
+  my_assert(eq_m128d031250(f) == 1);
+  my_assert(eq_m128d015625(f) != 1);
+
+  f = -64.031250f;
+  my_assert(eq_m64d062500(f) != 1);
+  my_assert(eq_m64d031250(f) == 1);
+  my_assert(eq_m64d015625(f) != 1);
+
+  f = -32.031250f;
+  my_assert(eq_m32d062500(f) != 1);
+  my_assert(eq_m32d031250(f) == 1);
+  my_assert(eq_m32d015625(f) != 1);
+
+  f = -16.031250f;
+  my_assert(eq_m16d062500(f) != 1);
+  my_assert(eq_m16d031250(f) == 1);
+  my_assert(eq_m16d015625(f) != 1);
+
+  f = -8.031250f;
+  my_assert(eq_m8d062500(f) != 1);
+  my_assert(eq_m8d031250(f) == 1);
+  my_assert(eq_m8d015625(f) != 1);
+
+  f = -4.031250f;
+  my_assert(eq_m4d062500(f) != 1);
+  my_assert(eq_m4d031250(f) == 1);
+  my_assert(eq_m4d015625(f) != 1);
+
+  f = -2.031250f;
+  my_assert(eq_m2d062500(f) != 1);
+  my_assert(eq_m2d031250(f) == 1);
+  my_assert(eq_m2d015625(f) != 1);
+
+  f = -1.031250f;
+  my_assert(eq_m1d062500(f) != 1);
+  my_assert(eq_m1d031250(f) == 1);
+  my_assert(eq_m1d015625(f) != 1);
+
+  f = -0.031250f;
+  my_assert(eq_m0d062500(f) != 1);
+  my_assert(eq_m0d031250(f) == 1);
+  my_assert(eq_m0d015625(f) != 1);
+
+  //--
+
+  f = 0.031250f;
+  my_assert(eq_0d015625(f) != 1);
+  my_assert(eq_0d031250(f) == 1);
+  my_assert(eq_0d062500(f) != 1);
+
+  f = 1.031250f;
+  my_assert(eq_1d015625(f) != 1);
+  my_assert(eq_1d031250(f) == 1);
+  my_assert(eq_1d062500(f) != 1);
+
+  f = 2.031250f;
+  my_assert(eq_2d015625(f) != 1);
+  my_assert(eq_2d031250(f) == 1);
+  my_assert(eq_2d062500(f) != 1);
+
+  f = 4.031250f;
+  my_assert(eq_4d015625(f) != 1);
+  my_assert(eq_4d031250(f) == 1);
+  my_assert(eq_4d062500(f) != 1);
+
+  f = 8.031250f;
+  my_assert(eq_8d015625(f) != 1);
+  my_assert(eq_8d031250(f) == 1);
+  my_assert(eq_8d062500(f) != 1);
+
+  f = 16.031250f;
+  my_assert(eq_16d015625(f) != 1);
+  my_assert(eq_16d031250(f) == 1);
+  my_assert(eq_16d062500(f) != 1);
+
+  f = 32.031250f;
+  my_assert(eq_32d015625(f) != 1);
+  my_assert(eq_32d031250(f) == 1);
+  my_assert(eq_32d062500(f) != 1);
+
+  f = 64.031250f;
+  my_assert(eq_64d015625(f) != 1);
+  my_assert(eq_64d031250(f) == 1);
+  my_assert(eq_64d062500(f) != 1);
+
+  f = 128.031250f;
+  my_assert(eq_128d015625(f) != 1);
+  my_assert(eq_128d031250(f) == 1);
+  my_assert(eq_128d062500(f) != 1);
+
+  f = 256.031250f;
+  my_assert(eq_256d015625(f) != 1);
+  my_assert(eq_256d031250(f) == 1);
+  my_assert(eq_256d062500(f) != 1);
+
+  f = 512.031250f;
+  my_assert(eq_512d015625(f) != 1);
+  my_assert(eq_512d031250(f) == 1);
+  my_assert(eq_512d062500(f) != 1);
+
+  f = 1024.031250f;
+  my_assert(eq_1024d015625(f) != 1);
+  my_assert(eq_1024d031250(f) == 1);
+  my_assert(eq_1024d062500(f) != 1);
+
+  f = 2048.031250f;
+  my_assert(eq_2048d015625(f) != 1);
+  my_assert(eq_2048d031250(f) == 1);
+  my_assert(eq_2048d062500(f) != 1);
+
+  f = 4096.031250f;
+  my_assert(eq_4096d015625(f) != 1);
+  my_assert(eq_4096d031250(f) == 1);
+  my_assert(eq_4096d062500(f) != 1);
+
+  f = 8192.031250f;
+  my_assert(eq_8192d015625(f) != 1);
+  my_assert(eq_8192d031250(f) == 1);
+  my_assert(eq_8192d062500(f) != 1);
+
+  f = 16384.031250f;
+  my_assert(eq_16384d015625(f) != 1);
+  my_assert(eq_16384d031250(f) == 1);
+  my_assert(eq_16384d062500(f) != 1);
+
+  f = 32768.031250f;
+  my_assert(eq_32768d015625(f) != 1);
+  my_assert(eq_32768d031250(f) == 1);
+  my_assert(eq_32768d062500(f) != 1);
+
+  f = 65536.031250f;
+  my_assert(eq_65536d015625(f) != 1);
+  my_assert(eq_65536d031250(f) == 1);
+  my_assert(eq_65536d062500(f) != 1);
+
+  f = 131072.031250f;
+  my_assert(eq_131072d015625(f) != 1);
+  my_assert(eq_131072d031250(f) == 1);
+  my_assert(eq_131072d062500(f) != 1);
+
+  return exit_status;
+}
