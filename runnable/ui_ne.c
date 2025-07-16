6a9f56b23695df9abcf1e3173af7c2295242ceaa
xyzzy <xyzzy@xyzzys-iMac.local>
2025-07-16 08:46:40 -0500

    ui_ne

---
+++ runnable/ui_ne.c
@@ -0,0 +1,297 @@
+
+
+int exit_status;
+#include "my_assert.h"
+
+int ne_00000000(unsigned int ui) { return ui != 0x00000000u; }
+int ne_00000001(unsigned int ui) { return ui != 0x00000001u; }
+int ne_00000002(unsigned int ui) { return ui != 0x00000002u; }
+
+int ne_00000003(unsigned int ui) { return ui != 0x00000003u; }
+int ne_00000004(unsigned int ui) { return ui != 0x00000004u; }
+int ne_00000005(unsigned int ui) { return ui != 0x00000005u; }
+
+int ne_00000007(unsigned int ui) { return ui != 0x00000007u; }
+int ne_00000008(unsigned int ui) { return ui != 0x00000008u; }
+int ne_00000009(unsigned int ui) { return ui != 0x00000009u; }
+
+int ne_0000000f(unsigned int ui) { return ui != 0x0000000fu; }
+int ne_00000010(unsigned int ui) { return ui != 0x00000010u; }
+int ne_00000011(unsigned int ui) { return ui != 0x00000011u; }
+
+int ne_0000001f(unsigned int ui) { return ui != 0x0000001fu; }
+int ne_00000020(unsigned int ui) { return ui != 0x00000020u; }
+int ne_00000021(unsigned int ui) { return ui != 0x00000021u; }
+
+int ne_0000003f(unsigned int ui) { return ui != 0x0000003fu; }
+int ne_00000040(unsigned int ui) { return ui != 0x00000040u; }
+int ne_00000041(unsigned int ui) { return ui != 0x00000041u; }
+
+int ne_0000007f(unsigned int ui) { return ui != 0x0000007fu; }
+int ne_00000080(unsigned int ui) { return ui != 0x00000080u; }
+int ne_00000081(unsigned int ui) { return ui != 0x00000081u; }
+
+int ne_000000ff(unsigned int ui) { return ui != 0x000000ffu; }
+int ne_00000100(unsigned int ui) { return ui != 0x00000100u; }
+int ne_00000101(unsigned int ui) { return ui != 0x00000101u; }
+
+int ne_000001ff(unsigned int ui) { return ui != 0x000001ffu; }
+int ne_00000200(unsigned int ui) { return ui != 0x00000200u; }
+int ne_00000201(unsigned int ui) { return ui != 0x00000201u; }
+
+int ne_000003ff(unsigned int ui) { return ui != 0x000003ffu; }
+int ne_00000400(unsigned int ui) { return ui != 0x00000400u; }
+int ne_00000401(unsigned int ui) { return ui != 0x00000401u; }
+
+int ne_000007ff(unsigned int ui) { return ui != 0x000007ffu; }
+int ne_00000800(unsigned int ui) { return ui != 0x00000800u; }
+int ne_00000801(unsigned int ui) { return ui != 0x00000801u; }
+
+int ne_00000fff(unsigned int ui) { return ui != 0x00000fffu; }
+int ne_00001000(unsigned int ui) { return ui != 0x00001000u; }
+int ne_00001001(unsigned int ui) { return ui != 0x00001001u; }
+
+int ne_00001fff(unsigned int ui) { return ui != 0x00001fffu; }
+int ne_00002000(unsigned int ui) { return ui != 0x00002000u; }
+int ne_00002001(unsigned int ui) { return ui != 0x00002001u; }
+
+int ne_00003fff(unsigned int ui) { return ui != 0x00003fffu; }
+int ne_00004000(unsigned int ui) { return ui != 0x00004000u; }
+int ne_00004001(unsigned int ui) { return ui != 0x00004001u; }
+
+int ne_00007fff(unsigned int ui) { return ui != 0x00007fffu; }
+int ne_00008000(unsigned int ui) { return ui != 0x00008000u; }
+int ne_00008001(unsigned int ui) { return ui != 0x00008001u; }
+
+int ne_0000ffff(unsigned int ui) { return ui != 0x0000ffffu; }
+int ne_00010000(unsigned int ui) { return ui != 0x00010000u; }
+int ne_00010001(unsigned int ui) { return ui != 0x00010001u; }
+
+int ne_0001ffff(unsigned int ui) { return ui != 0x0001ffffu; }
+int ne_00020000(unsigned int ui) { return ui != 0x00020000u; }
+int ne_00020001(unsigned int ui) { return ui != 0x00020001u; }
+
+int ne_0003ffff(unsigned int ui) { return ui != 0x0003ffffu; }
+int ne_00040000(unsigned int ui) { return ui != 0x00040000u; }
+int ne_00040001(unsigned int ui) { return ui != 0x00040001u; }
+
+int ne_0007ffff(unsigned int ui) { return ui != 0x0007ffffu; }
+int ne_00080000(unsigned int ui) { return ui != 0x00080000u; }
+int ne_00080001(unsigned int ui) { return ui != 0x00080001u; }
+
+int ne_000fffff(unsigned int ui) { return ui != 0x000fffffu; }
+int ne_00100000(unsigned int ui) { return ui != 0x00100000u; }
+int ne_00100001(unsigned int ui) { return ui != 0x00100001u; }
+
+int ne_001fffff(unsigned int ui) { return ui != 0x001fffffu; }
+int ne_00200000(unsigned int ui) { return ui != 0x00200000u; }
+int ne_00200001(unsigned int ui) { return ui != 0x00200001u; }
+
+int ne_003fffff(unsigned int ui) { return ui != 0x003fffffu; }
+int ne_00400000(unsigned int ui) { return ui != 0x00400000u; }
+int ne_00400001(unsigned int ui) { return ui != 0x00400001u; }
+
+int ne_007fffff(unsigned int ui) { return ui != 0x007fffffu; }
+int ne_00800000(unsigned int ui) { return ui != 0x00800000u; }
+int ne_00800001(unsigned int ui) { return ui != 0x00800001u; }
+
+int ne_00ffffff(unsigned int ui) { return ui != 0x00ffffffu; }
+int ne_01000000(unsigned int ui) { return ui != 0x01000000u; }
+int ne_01000001(unsigned int ui) { return ui != 0x01000001u; }
+
+int ne_01ffffff(unsigned int ui) { return ui != 0x01ffffffu; }
+int ne_02000000(unsigned int ui) { return ui != 0x02000000u; }
+int ne_02000001(unsigned int ui) { return ui != 0x02000001u; }
+
+int ne_03ffffff(unsigned int ui) { return ui != 0x03ffffffu; }
+int ne_04000000(unsigned int ui) { return ui != 0x04000000u; }
+int ne_04000001(unsigned int ui) { return ui != 0x04000001u; }
+
+int ne_07ffffff(unsigned int ui) { return ui != 0x07ffffffu; }
+int ne_08000000(unsigned int ui) { return ui != 0x08000000u; }
+int ne_08000001(unsigned int ui) { return ui != 0x08000001u; }
+
+int ne_0fffffff(unsigned int ui) { return ui != 0x0fffffffu; }
+int ne_10000000(unsigned int ui) { return ui != 0x10000000u; }
+int ne_10000001(unsigned int ui) { return ui != 0x10000001u; }
+
+int ne_1fffffff(unsigned int ui) { return ui != 0x1fffffffu; }
+int ne_20000000(unsigned int ui) { return ui != 0x20000000u; }
+int ne_20000001(unsigned int ui) { return ui != 0x20000001u; }
+
+int ne_3fffffff(unsigned int ui) { return ui != 0x3fffffffu; }
+int ne_40000000(unsigned int ui) { return ui != 0x40000000u; }
+int ne_40000001(unsigned int ui) { return ui != 0x40000001u; }
+
+int ne_7fffffff(unsigned int ui) { return ui != 0x7fffffffu; }
+int ne_80000000(unsigned int ui) { return ui != 0x80000000u; }
+int ne_80000001(unsigned int ui) { return ui != 0x80000001u; }
+
+static unsigned int ui;
+int main() {
+  ui = 0x00000000u;
+  my_assert(ne_00000000(ui) != 1);
+  my_assert(ne_00000001(ui) == 1);
+
+  ui = 0x00000001u;
+  my_assert(ne_00000000(ui) == 1);
+  my_assert(ne_00000001(ui) != 1);
+  my_assert(ne_00000002(ui) == 1);
+
+  ui = 0x00000002u;
+  my_assert(ne_00000001(ui) == 1);
+  my_assert(ne_00000002(ui) != 1);
+  my_assert(ne_00000003(ui) == 1);
+
+  ui = 0x00000004u;
+  my_assert(ne_00000003(ui) == 1);
+  my_assert(ne_00000004(ui) != 1);
+  my_assert(ne_00000005(ui) == 1);
+
+  ui = 0x00000008u;
+  my_assert(ne_00000007(ui) == 1);
+  my_assert(ne_00000008(ui) != 1);
+  my_assert(ne_00000009(ui) == 1);
+
+  ui = 0x00000010u;
+  my_assert(ne_0000000f(ui) == 1);
+  my_assert(ne_00000010(ui) != 1);
+  my_assert(ne_00000011(ui) == 1);
+
+  ui = 0x00000020u;
+  my_assert(ne_0000001f(ui) == 1);
+  my_assert(ne_00000020(ui) != 1);
+  my_assert(ne_00000021(ui) == 1);
+
+  ui = 0x00000040u;
+  my_assert(ne_0000003f(ui) == 1);
+  my_assert(ne_00000040(ui) != 1);
+  my_assert(ne_00000041(ui) == 1);
+
+  ui = 0x00000080u;
+  my_assert(ne_0000007f(ui) == 1);
+  my_assert(ne_00000080(ui) != 1);
+  my_assert(ne_00000081(ui) == 1);
+
+  ui = 0x00000100u;
+  my_assert(ne_000000ff(ui) == 1);
+  my_assert(ne_00000100(ui) != 1);
+  my_assert(ne_00000101(ui) == 1);
+
+  ui = 0x00000200u;
+  my_assert(ne_000001ff(ui) == 1);
+  my_assert(ne_00000200(ui) != 1);
+  my_assert(ne_00000201(ui) == 1);
+
+  ui = 0x00000400u;
+  my_assert(ne_000003ff(ui) == 1);
+  my_assert(ne_00000400(ui) != 1);
+  my_assert(ne_00000401(ui) == 1);
+
+  ui = 0x00000800u;
+  my_assert(ne_000007ff(ui) == 1);
+  my_assert(ne_00000800(ui) != 1);
+  my_assert(ne_00000801(ui) == 1);
+
+  ui = 0x00001000u;
+  my_assert(ne_00000fff(ui) == 1);
+  my_assert(ne_00001000(ui) != 1);
+  my_assert(ne_00001001(ui) == 1);
+
+  ui = 0x00002000u;
+  my_assert(ne_00001fff(ui) == 1);
+  my_assert(ne_00002000(ui) != 1);
+  my_assert(ne_00002001(ui) == 1);
+
+  ui = 0x00004000u;
+  my_assert(ne_00003fff(ui) == 1);
+  my_assert(ne_00004000(ui) != 1);
+  my_assert(ne_00004001(ui) == 1);
+
+  ui = 0x00008000u;
+  my_assert(ne_00007fff(ui) == 1);
+  my_assert(ne_00008000(ui) != 1);
+  my_assert(ne_00008001(ui) == 1);
+
+  ui = 0x00010000u;
+  my_assert(ne_0000ffff(ui) == 1);
+  my_assert(ne_00010000(ui) != 1);
+  my_assert(ne_00010001(ui) == 1);
+
+  ui = 0x00020000u;
+  my_assert(ne_0001ffff(ui) == 1);
+  my_assert(ne_00020000(ui) != 1);
+  my_assert(ne_00020001(ui) == 1);
+
+  ui = 0x00040000u;
+  my_assert(ne_0003ffff(ui) == 1);
+  my_assert(ne_00040000(ui) != 1);
+  my_assert(ne_00040001(ui) == 1);
+
+  ui = 0x00080000u;
+  my_assert(ne_0007ffff(ui) == 1);
+  my_assert(ne_00080000(ui) != 1);
+  my_assert(ne_00080001(ui) == 1);
+
+  ui = 0x00100000u;
+  my_assert(ne_000fffff(ui) == 1);
+  my_assert(ne_00100000(ui) != 1);
+  my_assert(ne_00100001(ui) == 1);
+
+  ui = 0x00200000u;
+  my_assert(ne_001fffff(ui) == 1);
+  my_assert(ne_00200000(ui) != 1);
+  my_assert(ne_00200001(ui) == 1);
+
+  ui = 0x00400000u;
+  my_assert(ne_003fffff(ui) == 1);
+  my_assert(ne_00400000(ui) != 1);
+  my_assert(ne_00400001(ui) == 1);
+
+  ui = 0x00800000u;
+  my_assert(ne_007fffff(ui) == 1);
+  my_assert(ne_00800000(ui) != 1);
+  my_assert(ne_00800001(ui) == 1);
+
+  ui = 0x01000000u;
+  my_assert(ne_00ffffff(ui) == 1);
+  my_assert(ne_01000000(ui) != 1);
+  my_assert(ne_01000001(ui) == 1);
+
+  ui = 0x02000000u;
+  my_assert(ne_01ffffff(ui) == 1);
+  my_assert(ne_02000000(ui) != 1);
+  my_assert(ne_02000001(ui) == 1);
+
+  ui = 0x04000000u;
+  my_assert(ne_03ffffff(ui) == 1);
+  my_assert(ne_04000000(ui) != 1);
+  my_assert(ne_04000001(ui) == 1);
+
+  ui = 0x08000000u;
+  my_assert(ne_07ffffff(ui) == 1);
+  my_assert(ne_08000000(ui) != 1);
+  my_assert(ne_08000001(ui) == 1);
+
+  ui = 0x10000000u;
+  my_assert(ne_0fffffff(ui) == 1);
+  my_assert(ne_10000000(ui) != 1);
+  my_assert(ne_10000001(ui) == 1);
+
+  ui = 0x20000000u;
+  my_assert(ne_1fffffff(ui) == 1);
+  my_assert(ne_20000000(ui) != 1);
+  my_assert(ne_20000001(ui) == 1);
+
+  ui = 0x40000000u;
+  my_assert(ne_3fffffff(ui) == 1);
+  my_assert(ne_40000000(ui) != 1);
+  my_assert(ne_40000001(ui) == 1);
+
+  ui = 0x80000000u;
+  my_assert(ne_7fffffff(ui) == 1);
+  my_assert(ne_80000000(ui) != 1);
+  my_assert(ne_80000001(ui) == 1);
+
+  return exit_status;
+}
