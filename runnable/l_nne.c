7fe8f03151e546f8bfde31c603e2e50c9608c950
xyzzy <xyzzy@xyzzys-iMac.local>
2025-07-17 21:23:08 -0500

    l_nne

---
+++ runnable/l_nne.c
@@ -0,0 +1,507 @@
+
+
+int exit_status;
+
+#include "my_assert.h"
+
+int nne_m1073741825(signed long sl) { return !(sl != -1073741825); }
+int nne_m1073741824(signed long sl) { return !(sl != -1073741824); }
+int nne_m1073741823(signed long sl) { return !(sl != -1073741823); }
+int nne_m536870913(signed long sl) { return !(sl != -536870913); }
+int nne_m536870912(signed long sl) { return !(sl != -536870912); }
+int nne_m536870911(signed long sl) { return !(sl != -536870911); }
+int nne_m268435457(signed long sl) { return !(sl != -268435457); }
+int nne_m268435456(signed long sl) { return !(sl != -268435456); }
+int nne_m268435455(signed long sl) { return !(sl != -268435455); }
+int nne_m134217729(signed long sl) { return !(sl != -134217729); }
+int nne_m134217728(signed long sl) { return !(sl != -134217728); }
+int nne_m134217727(signed long sl) { return !(sl != -134217727); }
+int nne_m67108865(signed long sl) { return !(sl != -67108865); }
+int nne_m67108864(signed long sl) { return !(sl != -67108864); }
+int nne_m67108863(signed long sl) { return !(sl != -67108863); }
+int nne_m33554433(signed long sl) { return !(sl != -33554433); }
+int nne_m33554432(signed long sl) { return !(sl != -33554432); }
+int nne_m33554431(signed long sl) { return !(sl != -33554431); }
+int nne_m16777217(signed long sl) { return !(sl != -16777217); }
+int nne_m16777216(signed long sl) { return !(sl != -16777216); }
+int nne_m16777215(signed long sl) { return !(sl != -16777215); }
+int nne_m8388609(signed long sl) { return !(sl != -8388609); }
+int nne_m8388608(signed long sl) { return !(sl != -8388608); }
+int nne_m8388607(signed long sl) { return !(sl != -8388607); }
+int nne_m4194305(signed long sl) { return !(sl != -4194305); }
+int nne_m4194304(signed long sl) { return !(sl != -4194304); }
+int nne_m4194303(signed long sl) { return !(sl != -4194303); }
+int nne_m2097153(signed long sl) { return !(sl != -2097153); }
+int nne_m2097152(signed long sl) { return !(sl != -2097152); }
+int nne_m2097151(signed long sl) { return !(sl != -2097151); }
+int nne_m1048577(signed long sl) { return !(sl != -1048577); }
+int nne_m1048576(signed long sl) { return !(sl != -1048576); }
+int nne_m1048575(signed long sl) { return !(sl != -1048575); }
+int nne_m524289(signed long sl) { return !(sl != -524289); }
+int nne_m524288(signed long sl) { return !(sl != -524288); }
+int nne_m524287(signed long sl) { return !(sl != -524287); }
+int nne_m262145(signed long sl) { return !(sl != -262145); }
+int nne_m262144(signed long sl) { return !(sl != -262144); }
+int nne_m262143(signed long sl) { return !(sl != -262143); }
+int nne_m131073(signed long sl) { return !(sl != -131073); }
+int nne_m131072(signed long sl) { return !(sl != -131072); }
+int nne_m131071(signed long sl) { return !(sl != -131071); }
+int nne_m65536(signed long sl) { return !(sl != -65536); }
+int nne_m65535(signed long sl) { return !(sl != -65535); }
+int nne_m65534(signed long sl) { return !(sl != -65534); }
+int nne_m32769(signed long sl) { return !(sl != -32769); }
+int nne_m32768(signed long sl) { return !(sl != -32768); }
+int nne_m32767(signed long sl) { return !(sl != -32767); }
+int nne_m16385(signed long sl) { return !(sl != -16385); }
+int nne_m16384(signed long sl) { return !(sl != -16384); }
+int nne_m16383(signed long sl) { return !(sl != -16383); }
+int nne_m8193(signed long sl) { return !(sl != -8193); }
+int nne_m8192(signed long sl) { return !(sl != -8192); }
+int nne_m8191(signed long sl) { return !(sl != -8191); }
+int nne_m4097(signed long sl) { return !(sl != -4097); }
+int nne_m4096(signed long sl) { return !(sl != -4096); }
+int nne_m4095(signed long sl) { return !(sl != -4095); }
+int nne_m2049(signed long sl) { return !(sl != -2049); }
+int nne_m2048(signed long sl) { return !(sl != -2048); }
+int nne_m2047(signed long sl) { return !(sl != -2047); }
+int nne_m1025(signed long sl) { return !(sl != -1025); }
+int nne_m1024(signed long sl) { return !(sl != -1024); }
+int nne_m1023(signed long sl) { return !(sl != -1023); }
+int nne_m513(signed long sl) { return !(sl != -513); }
+int nne_m512(signed long sl) { return !(sl != -512); }
+int nne_m511(signed long sl) { return !(sl != -511); }
+int nne_m257(signed long sl) { return !(sl != -257); }
+int nne_m256(signed long sl) { return !(sl != -256); }
+int nne_m255(signed long sl) { return !(sl != -255); }
+int nne_m129(signed long sl) { return !(sl != -129); }
+int nne_m128(signed long sl) { return !(sl != -128); }
+int nne_m127(signed long sl) { return !(sl != -127); }
+int nne_m65(signed long sl) { return !(sl != -65); }
+int nne_m64(signed long sl) { return !(sl != -64); }
+int nne_m63(signed long sl) { return !(sl != -63); }
+int nne_m33(signed long sl) { return !(sl != -33); }
+int nne_m32(signed long sl) { return !(sl != -32); }
+int nne_m31(signed long sl) { return !(sl != -31); }
+int nne_m17(signed long sl) { return !(sl != -17); }
+int nne_m16(signed long sl) { return !(sl != -16); }
+int nne_m15(signed long sl) { return !(sl != -15); }
+int nne_m9(signed long sl) { return !(sl != -9); }
+int nne_m8(signed long sl) { return !(sl != -8); }
+int nne_m7(signed long sl) { return !(sl != -7); }
+int nne_m5(signed long sl) { return !(sl != -5); }
+int nne_m4(signed long sl) { return !(sl != -4); }
+int nne_m3(signed long sl) { return !(sl != -3); }
+int nne_m2(signed long sl) { return !(sl != -2); }
+int nne_m1(signed long sl) { return !(sl != -1); }
+int nne_00(signed long sl) { return !(sl != 0); }
+int nne_01(signed long sl) { return !(sl != 1); }
+int nne_02(signed long sl) { return !(sl != 2); }
+int nne_03(signed long sl) { return !(sl != 3); }
+int nne_04(signed long sl) { return !(sl != 4); }
+int nne_05(signed long sl) { return !(sl != 5); }
+int nne_07(signed long sl) { return !(sl != 7); }
+int nne_08(signed long sl) { return !(sl != 8); }
+int nne_09(signed long sl) { return !(sl != 9); }
+int nne_15(signed long sl) { return !(sl != 15); }
+int nne_16(signed long sl) { return !(sl != 16); }
+int nne_17(signed long sl) { return !(sl != 17); }
+int nne_31(signed long sl) { return !(sl != 31); }
+int nne_32(signed long sl) { return !(sl != 32); }
+int nne_33(signed long sl) { return !(sl != 33); }
+int nne_63(signed long sl) { return !(sl != 63); }
+int nne_64(signed long sl) { return !(sl != 64); }
+int nne_65(signed long sl) { return !(sl != 65); }
+int nne_127(signed long sl) { return !(sl != 127); }
+int nne_128(signed long sl) { return !(sl != 128); }
+int nne_129(signed long sl) { return !(sl != 129); }
+int nne_255(signed long sl) { return !(sl != 255); }
+int nne_256(signed long sl) { return !(sl != 256); }
+int nne_257(signed long sl) { return !(sl != 257); }
+int nne_511(signed long sl) { return !(sl != 511); }
+int nne_512(signed long sl) { return !(sl != 512); }
+int nne_513(signed long sl) { return !(sl != 513); }
+int nne_1023(signed long sl) { return !(sl != 1023); }
+int nne_1024(signed long sl) { return !(sl != 1024); }
+int nne_1025(signed long sl) { return !(sl != 1025); }
+int nne_2047(signed long sl) { return !(sl != 2047); }
+int nne_2048(signed long sl) { return !(sl != 2048); }
+int nne_2049(signed long sl) { return !(sl != 2049); }
+int nne_4095(signed long sl) { return !(sl != 4095); }
+int nne_4096(signed long sl) { return !(sl != 4096); }
+int nne_4097(signed long sl) { return !(sl != 4097); }
+int nne_8191(signed long sl) { return !(sl != 8191); }
+int nne_8192(signed long sl) { return !(sl != 8192); }
+int nne_8193(signed long sl) { return !(sl != 8193); }
+int nne_16383(signed long sl) { return !(sl != 16383); }
+int nne_16384(signed long sl) { return !(sl != 16384); }
+int nne_16385(signed long sl) { return !(sl != 16385); }
+int nne_32767(signed long sl) { return !(sl != 32767); }
+int nne_32768(signed long sl) { return !(sl != 32768); }
+int nne_32769(signed long sl) { return !(sl != 32769); }
+int nne_65534(signed long sl) { return !(sl != 65534); }
+int nne_65535(signed long sl) { return !(sl != 65535); }
+int nne_65536(signed long sl) { return !(sl != 65536); }
+int nne_65537(signed long sl) { return !(sl != 65537); }
+int nne_131071(signed long sl) { return !(sl != 131071); }
+int nne_131072(signed long sl) { return !(sl != 131072); }
+int nne_131073(signed long sl) { return !(sl != 131073); }
+int nne_262143(signed long sl) { return !(sl != 262143); }
+int nne_262144(signed long sl) { return !(sl != 262144); }
+int nne_262145(signed long sl) { return !(sl != 262145); }
+int nne_524287(signed long sl) { return !(sl != 524287); }
+int nne_524288(signed long sl) { return !(sl != 524288); }
+int nne_524289(signed long sl) { return !(sl != 524289); }
+int nne_1048575(signed long sl) { return !(sl != 1048575); }
+int nne_1048576(signed long sl) { return !(sl != 1048576); }
+int nne_1048577(signed long sl) { return !(sl != 1048577); }
+int nne_2097151(signed long sl) { return !(sl != 2097151); }
+int nne_2097152(signed long sl) { return !(sl != 2097152); }
+int nne_2097153(signed long sl) { return !(sl != 2097153); }
+int nne_4194303(signed long sl) { return !(sl != 4194303); }
+int nne_4194304(signed long sl) { return !(sl != 4194304); }
+int nne_4194305(signed long sl) { return !(sl != 4194305); }
+int nne_8388607(signed long sl) { return !(sl != 8388607); }
+int nne_8388608(signed long sl) { return !(sl != 8388608); }
+int nne_8388609(signed long sl) { return !(sl != 8388609); }
+int nne_16777215(signed long sl) { return !(sl != 16777215); }
+int nne_16777216(signed long sl) { return !(sl != 16777216); }
+int nne_16777217(signed long sl) { return !(sl != 16777217); }
+int nne_33554431(signed long sl) { return !(sl != 33554431); }
+int nne_33554432(signed long sl) { return !(sl != 33554432); }
+int nne_33554433(signed long sl) { return !(sl != 33554433); }
+int nne_67108863(signed long sl) { return !(sl != 67108863); }
+int nne_67108864(signed long sl) { return !(sl != 67108864); }
+int nne_67108865(signed long sl) { return !(sl != 67108865); }
+int nne_134217727(signed long sl) { return !(sl != 134217727); }
+int nne_134217728(signed long sl) { return !(sl != 134217728); }
+int nne_134217729(signed long sl) { return !(sl != 134217729); }
+int nne_268435455(signed long sl) { return !(sl != 268435455); }
+int nne_268435456(signed long sl) { return !(sl != 268435456); }
+int nne_268435457(signed long sl) { return !(sl != 268435457); }
+int nne_536870911(signed long sl) { return !(sl != 536870911); }
+int nne_536870912(signed long sl) { return !(sl != 536870912); }
+int nne_536870913(signed long sl) { return !(sl != 536870913); }
+int nne_1073741823(signed long sl) { return !(sl != 1073741823); }
+int nne_1073741824(signed long sl) { return !(sl != 1073741824); }
+int nne_1073741825(signed long sl) { return !(sl != 1073741825); }
+
+static signed long sl;
+int main() {
+
+  sl = -1073741824;
+  my_assert(nne_m1073741825(sl) != 1);
+  my_assert(nne_m1073741824(sl) == 1);
+  my_assert(nne_m1073741823(sl) != 1);
+
+  sl = -536870912;
+  my_assert(nne_m536870913(sl) != 1);
+  my_assert(nne_m536870912(sl) == 1);
+  my_assert(nne_m536870911(sl) != 1);
+
+  sl = -268435456;
+  my_assert(nne_m268435457(sl) != 1);
+  my_assert(nne_m268435456(sl) == 1);
+  my_assert(nne_m268435455(sl) != 1);
+
+  sl = -134217728;
+  my_assert(nne_m134217729(sl) != 1);
+  my_assert(nne_m134217728(sl) == 1);
+  my_assert(nne_m134217727(sl) != 1);
+
+  sl = -67108864;
+  my_assert(nne_m67108865(sl) != 1);
+  my_assert(nne_m67108864(sl) == 1);
+  my_assert(nne_m67108863(sl) != 1);
+
+  sl = -33554432;
+  my_assert(nne_m33554433(sl) != 1);
+  my_assert(nne_m33554432(sl) == 1);
+  my_assert(nne_m33554431(sl) != 1);
+
+  sl = -16777216;
+  my_assert(nne_m16777217(sl) != 1);
+  my_assert(nne_m16777216(sl) == 1);
+  my_assert(nne_m16777215(sl) != 1);
+
+  sl = -8388608;
+  my_assert(nne_m8388609(sl) != 1);
+  my_assert(nne_m8388608(sl) == 1);
+  my_assert(nne_m8388607(sl) != 1);
+
+  sl = -4194304;
+  my_assert(nne_m4194305(sl) != 1);
+  my_assert(nne_m4194304(sl) == 1);
+  my_assert(nne_m4194303(sl) != 1);
+
+  sl = -2097152;
+  my_assert(nne_m2097153(sl) != 1);
+  my_assert(nne_m2097152(sl) == 1);
+  my_assert(nne_m2097151(sl) != 1);
+
+  sl = -1048576;
+  my_assert(nne_m1048577(sl) != 1);
+  my_assert(nne_m1048576(sl) == 1);
+  my_assert(nne_m1048575(sl) != 1);
+
+  sl = -524288;
+  my_assert(nne_m524289(sl) != 1);
+  my_assert(nne_m524288(sl) == 1);
+  my_assert(nne_m524287(sl) != 1);
+
+  sl = -262144;
+  my_assert(nne_m262145(sl) != 1);
+  my_assert(nne_m262144(sl) == 1);
+  my_assert(nne_m262143(sl) != 1);
+
+  sl = -131072;
+  my_assert(nne_m131073(sl) != 1);
+  my_assert(nne_m131072(sl) == 1);
+  my_assert(nne_m131071(sl) != 1);
+
+  sl = -65535;
+  my_assert(nne_m65536(sl) != 1);
+  my_assert(nne_m65535(sl) == 1);
+  my_assert(nne_m65534(sl) != 1);
+
+  sl = -32768;
+  my_assert(nne_m32769(sl) != 1);
+  my_assert(nne_m32768(sl) == 1);
+  my_assert(nne_m32767(sl) != 1);
+
+  sl = -16384;
+  my_assert(nne_m16385(sl) != 1);
+  my_assert(nne_m16384(sl) == 1);
+  my_assert(nne_m16383(sl) != 1);
+
+  sl = -8192;
+  my_assert(nne_m8193(sl) != 1);
+  my_assert(nne_m8192(sl) == 1);
+  my_assert(nne_m8191(sl) != 1);
+
+  sl = -4096;
+  my_assert(nne_m4097(sl) != 1);
+  my_assert(nne_m4096(sl) == 1);
+  my_assert(nne_m4095(sl) != 1);
+
+  sl = -2048;
+  my_assert(nne_m2049(sl) != 1);
+  my_assert(nne_m2048(sl) == 1);
+  my_assert(nne_m2047(sl) != 1);
+
+  sl = -1024;
+  my_assert(nne_m1025(sl) != 1);
+  my_assert(nne_m1024(sl) == 1);
+  my_assert(nne_m1023(sl) != 1);
+
+  sl = -512;
+  my_assert(nne_m513(sl) != 1);
+  my_assert(nne_m512(sl) == 1);
+  my_assert(nne_m511(sl) != 1);
+
+  sl = -256;
+  my_assert(nne_m257(sl) != 1);
+  my_assert(nne_m256(sl) == 1);
+  my_assert(nne_m255(sl) != 1);
+
+  sl = -128;
+  my_assert(nne_m129(sl) != 1);
+  my_assert(nne_m128(sl) == 1);
+  my_assert(nne_m127(sl) != 1);
+
+  sl = -64;
+  my_assert(nne_m65(sl) != 1);
+  my_assert(nne_m64(sl) == 1);
+  my_assert(nne_m63(sl) != 1);
+
+  sl = -32;
+  my_assert(nne_m33(sl) != 1);
+  my_assert(nne_m32(sl) == 1);
+  my_assert(nne_m31(sl) != 1);
+
+  sl = -16;
+  my_assert(nne_m17(sl) != 1);
+  my_assert(nne_m16(sl) == 1);
+  my_assert(nne_m15(sl) != 1);
+
+  sl = -8;
+  my_assert(nne_m9(sl) != 1);
+  my_assert(nne_m8(sl) == 1);
+  my_assert(nne_m7(sl) != 1);
+
+  sl = -4;
+  my_assert(nne_m5(sl) != 1);
+  my_assert(nne_m4(sl) == 1);
+  my_assert(nne_m3(sl) != 1);
+
+  sl = -2;
+  my_assert(nne_m3(sl) != 1);
+  my_assert(nne_m2(sl) == 1);
+  my_assert(nne_m1(sl) != 1);
+
+  sl = -1;
+  my_assert(nne_m2(sl) != 1);
+  my_assert(nne_m1(sl) == 1);
+  my_assert(nne_00(sl) != 1);
+
+  sl = 0;
+  my_assert(nne_m1(sl) != 1);
+  my_assert(nne_00(sl) == 1);
+  my_assert(nne_01(sl) != 1);
+
+  sl = 1;
+  my_assert(nne_00(sl) != 1);
+  my_assert(nne_01(sl) == 1);
+  my_assert(nne_02(sl) != 1);
+
+  sl = 2;
+  my_assert(nne_01(sl) != 1);
+  my_assert(nne_02(sl) == 1);
+  my_assert(nne_03(sl) != 1);
+
+  sl = 4;
+  my_assert(nne_03(sl) != 1);
+  my_assert(nne_04(sl) == 1);
+  my_assert(nne_05(sl) != 1);
+
+  sl = 8;
+  my_assert(nne_07(sl) != 1);
+  my_assert(nne_08(sl) == 1);
+  my_assert(nne_09(sl) != 1);
+
+  sl = 16;
+  my_assert(nne_15(sl) != 1);
+  my_assert(nne_16(sl) == 1);
+  my_assert(nne_17(sl) != 1);
+
+  sl = 32;
+  my_assert(nne_31(sl) != 1);
+  my_assert(nne_32(sl) == 1);
+  my_assert(nne_33(sl) != 1);
+
+  sl = 64;
+  my_assert(nne_63(sl) != 1);
+  my_assert(nne_64(sl) == 1);
+  my_assert(nne_65(sl) != 1);
+
+  sl = 128;
+  my_assert(nne_127(sl) != 1);
+  my_assert(nne_128(sl) == 1);
+  my_assert(nne_129(sl) != 1);
+
+  sl = 256;
+  my_assert(nne_255(sl) != 1);
+  my_assert(nne_256(sl) == 1);
+  my_assert(nne_257(sl) != 1);
+
+  sl = 512;
+  my_assert(nne_511(sl) != 1);
+  my_assert(nne_512(sl) == 1);
+  my_assert(nne_513(sl) != 1);
+
+  sl = 1024;
+  my_assert(nne_1023(sl) != 1);
+  my_assert(nne_1024(sl) == 1);
+  my_assert(nne_1025(sl) != 1);
+
+  sl = 2048;
+  my_assert(nne_2047(sl) != 1);
+  my_assert(nne_2048(sl) == 1);
+  my_assert(nne_2049(sl) != 1);
+
+  sl = 4096;
+  my_assert(nne_4095(sl) != 1);
+  my_assert(nne_4096(sl) == 1);
+  my_assert(nne_4097(sl) != 1);
+
+  sl = 8192;
+  my_assert(nne_8191(sl) != 1);
+  my_assert(nne_8192(sl) == 1);
+  my_assert(nne_8193(sl) != 1);
+
+  sl = 16384;
+  my_assert(nne_16383(sl) != 1);
+  my_assert(nne_16384(sl) == 1);
+  my_assert(nne_16385(sl) != 1);
+
+  sl = 32768;
+  my_assert(nne_32767(sl) != 1);
+  my_assert(nne_32768(sl) == 1);
+  my_assert(nne_32769(sl) != 1);
+
+  sl = 65536;
+  my_assert(nne_65535(sl) != 1);
+  my_assert(nne_65536(sl) == 1);
+  my_assert(nne_65537(sl) != 1);
+
+  sl = 131072;
+  my_assert(nne_131071(sl) != 1);
+  my_assert(nne_131072(sl) == 1);
+  my_assert(nne_131073(sl) != 1);
+
+  sl = 262144;
+  my_assert(nne_262143(sl) != 1);
+  my_assert(nne_262144(sl) == 1);
+  my_assert(nne_262145(sl) != 1);
+
+  sl = 524288;
+  my_assert(nne_524287(sl) != 1);
+  my_assert(nne_524288(sl) == 1);
+  my_assert(nne_524289(sl) != 1);
+
+  sl = 1048576;
+  my_assert(nne_1048575(sl) != 1);
+  my_assert(nne_1048576(sl) == 1);
+  my_assert(nne_1048577(sl) != 1);
+
+  sl = 2097152;
+  my_assert(nne_2097151(sl) != 1);
+  my_assert(nne_2097152(sl) == 1);
+  my_assert(nne_2097153(sl) != 1);
+
+  sl = 4194304;
+  my_assert(nne_4194303(sl) != 1);
+  my_assert(nne_4194304(sl) == 1);
+  my_assert(nne_4194305(sl) != 1);
+
+  sl = 8388608;
+  my_assert(nne_8388607(sl) != 1);
+  my_assert(nne_8388608(sl) == 1);
+  my_assert(nne_8388609(sl) != 1);
+
+  sl = 16777216;
+  my_assert(nne_16777215(sl) != 1);
+  my_assert(nne_16777216(sl) == 1);
+  my_assert(nne_16777217(sl) != 1);
+
+  sl = 33554432;
+  my_assert(nne_33554431(sl) != 1);
+  my_assert(nne_33554432(sl) == 1);
+  my_assert(nne_33554433(sl) != 1);
+
+  sl = 67108864;
+  my_assert(nne_67108863(sl) != 1);
+  my_assert(nne_67108864(sl) == 1);
+  my_assert(nne_67108865(sl) != 1);
+
+  sl = 134217728;
+  my_assert(nne_134217727(sl) != 1);
+  my_assert(nne_134217728(sl) == 1);
+  my_assert(nne_134217729(sl) != 1);
+
+  sl = 268435456;
+  my_assert(nne_268435455(sl) != 1);
+  my_assert(nne_268435456(sl) == 1);
+  my_assert(nne_268435457(sl) != 1);
+
+  sl = 536870912;
+  my_assert(nne_536870911(sl) != 1);
+  my_assert(nne_536870912(sl) == 1);
+  my_assert(nne_536870913(sl) != 1);
+
+  sl = 1073741824;
+  my_assert(nne_1073741823(sl) != 1);
+  my_assert(nne_1073741824(sl) == 1);
+  my_assert(nne_1073741825(sl) != 1);
+
+  return exit_status;
+}
