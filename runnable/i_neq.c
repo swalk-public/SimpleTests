352552d1d6080f93664678d2f0c0524f0a5fe02c
xyzzy <xyzzy@xyzzys-iMac.local>
2025-07-20 20:05:53 -0500

    i_neq

---
+++ runnable/i_neq.c
@@ -0,0 +1,507 @@
+
+
+int exit_status;
+
+#include "my_assert.h"
+
+int neq_m1073741825(signed int si) { return !(si == -1073741825); }
+int neq_m1073741824(signed int si) { return !(si == -1073741824); }
+int neq_m1073741823(signed int si) { return !(si == -1073741823); }
+int neq_m536870913(signed int si) { return !(si == -536870913); }
+int neq_m536870912(signed int si) { return !(si == -536870912); }
+int neq_m536870911(signed int si) { return !(si == -536870911); }
+int neq_m268435457(signed int si) { return !(si == -268435457); }
+int neq_m268435456(signed int si) { return !(si == -268435456); }
+int neq_m268435455(signed int si) { return !(si == -268435455); }
+int neq_m134217729(signed int si) { return !(si == -134217729); }
+int neq_m134217728(signed int si) { return !(si == -134217728); }
+int neq_m134217727(signed int si) { return !(si == -134217727); }
+int neq_m67108865(signed int si) { return !(si == -67108865); }
+int neq_m67108864(signed int si) { return !(si == -67108864); }
+int neq_m67108863(signed int si) { return !(si == -67108863); }
+int neq_m33554433(signed int si) { return !(si == -33554433); }
+int neq_m33554432(signed int si) { return !(si == -33554432); }
+int neq_m33554431(signed int si) { return !(si == -33554431); }
+int neq_m16777217(signed int si) { return !(si == -16777217); }
+int neq_m16777216(signed int si) { return !(si == -16777216); }
+int neq_m16777215(signed int si) { return !(si == -16777215); }
+int neq_m8388609(signed int si) { return !(si == -8388609); }
+int neq_m8388608(signed int si) { return !(si == -8388608); }
+int neq_m8388607(signed int si) { return !(si == -8388607); }
+int neq_m4194305(signed int si) { return !(si == -4194305); }
+int neq_m4194304(signed int si) { return !(si == -4194304); }
+int neq_m4194303(signed int si) { return !(si == -4194303); }
+int neq_m2097153(signed int si) { return !(si == -2097153); }
+int neq_m2097152(signed int si) { return !(si == -2097152); }
+int neq_m2097151(signed int si) { return !(si == -2097151); }
+int neq_m1048577(signed int si) { return !(si == -1048577); }
+int neq_m1048576(signed int si) { return !(si == -1048576); }
+int neq_m1048575(signed int si) { return !(si == -1048575); }
+int neq_m524289(signed int si) { return !(si == -524289); }
+int neq_m524288(signed int si) { return !(si == -524288); }
+int neq_m524287(signed int si) { return !(si == -524287); }
+int neq_m262145(signed int si) { return !(si == -262145); }
+int neq_m262144(signed int si) { return !(si == -262144); }
+int neq_m262143(signed int si) { return !(si == -262143); }
+int neq_m131073(signed int si) { return !(si == -131073); }
+int neq_m131072(signed int si) { return !(si == -131072); }
+int neq_m131071(signed int si) { return !(si == -131071); }
+int neq_m65536(signed int si) { return !(si == -65536); }
+int neq_m65535(signed int si) { return !(si == -65535); }
+int neq_m65534(signed int si) { return !(si == -65534); }
+int neq_m32769(signed int si) { return !(si == -32769); }
+int neq_m32768(signed int si) { return !(si == -32768); }
+int neq_m32767(signed int si) { return !(si == -32767); }
+int neq_m16385(signed int si) { return !(si == -16385); }
+int neq_m16384(signed int si) { return !(si == -16384); }
+int neq_m16383(signed int si) { return !(si == -16383); }
+int neq_m8193(signed int si) { return !(si == -8193); }
+int neq_m8192(signed int si) { return !(si == -8192); }
+int neq_m8191(signed int si) { return !(si == -8191); }
+int neq_m4097(signed int si) { return !(si == -4097); }
+int neq_m4096(signed int si) { return !(si == -4096); }
+int neq_m4095(signed int si) { return !(si == -4095); }
+int neq_m2049(signed int si) { return !(si == -2049); }
+int neq_m2048(signed int si) { return !(si == -2048); }
+int neq_m2047(signed int si) { return !(si == -2047); }
+int neq_m1025(signed int si) { return !(si == -1025); }
+int neq_m1024(signed int si) { return !(si == -1024); }
+int neq_m1023(signed int si) { return !(si == -1023); }
+int neq_m513(signed int si) { return !(si == -513); }
+int neq_m512(signed int si) { return !(si == -512); }
+int neq_m511(signed int si) { return !(si == -511); }
+int neq_m257(signed int si) { return !(si == -257); }
+int neq_m256(signed int si) { return !(si == -256); }
+int neq_m255(signed int si) { return !(si == -255); }
+int neq_m129(signed int si) { return !(si == -129); }
+int neq_m128(signed int si) { return !(si == -128); }
+int neq_m127(signed int si) { return !(si == -127); }
+int neq_m65(signed int si) { return !(si == -65); }
+int neq_m64(signed int si) { return !(si == -64); }
+int neq_m63(signed int si) { return !(si == -63); }
+int neq_m33(signed int si) { return !(si == -33); }
+int neq_m32(signed int si) { return !(si == -32); }
+int neq_m31(signed int si) { return !(si == -31); }
+int neq_m17(signed int si) { return !(si == -17); }
+int neq_m16(signed int si) { return !(si == -16); }
+int neq_m15(signed int si) { return !(si == -15); }
+int neq_m9(signed int si) { return !(si == -9); }
+int neq_m8(signed int si) { return !(si == -8); }
+int neq_m7(signed int si) { return !(si == -7); }
+int neq_m5(signed int si) { return !(si == -5); }
+int neq_m4(signed int si) { return !(si == -4); }
+int neq_m3(signed int si) { return !(si == -3); }
+int neq_m2(signed int si) { return !(si == -2); }
+int neq_m1(signed int si) { return !(si == -1); }
+int neq_00(signed int si) { return !(si == 0); }
+int neq_01(signed int si) { return !(si == 1); }
+int neq_02(signed int si) { return !(si == 2); }
+int neq_03(signed int si) { return !(si == 3); }
+int neq_04(signed int si) { return !(si == 4); }
+int neq_05(signed int si) { return !(si == 5); }
+int neq_07(signed int si) { return !(si == 7); }
+int neq_08(signed int si) { return !(si == 8); }
+int neq_09(signed int si) { return !(si == 9); }
+int neq_15(signed int si) { return !(si == 15); }
+int neq_16(signed int si) { return !(si == 16); }
+int neq_17(signed int si) { return !(si == 17); }
+int neq_31(signed int si) { return !(si == 31); }
+int neq_32(signed int si) { return !(si == 32); }
+int neq_33(signed int si) { return !(si == 33); }
+int neq_63(signed int si) { return !(si == 63); }
+int neq_64(signed int si) { return !(si == 64); }
+int neq_65(signed int si) { return !(si == 65); }
+int neq_127(signed int si) { return !(si == 127); }
+int neq_128(signed int si) { return !(si == 128); }
+int neq_129(signed int si) { return !(si == 129); }
+int neq_255(signed int si) { return !(si == 255); }
+int neq_256(signed int si) { return !(si == 256); }
+int neq_257(signed int si) { return !(si == 257); }
+int neq_511(signed int si) { return !(si == 511); }
+int neq_512(signed int si) { return !(si == 512); }
+int neq_513(signed int si) { return !(si == 513); }
+int neq_1023(signed int si) { return !(si == 1023); }
+int neq_1024(signed int si) { return !(si == 1024); }
+int neq_1025(signed int si) { return !(si == 1025); }
+int neq_2047(signed int si) { return !(si == 2047); }
+int neq_2048(signed int si) { return !(si == 2048); }
+int neq_2049(signed int si) { return !(si == 2049); }
+int neq_4095(signed int si) { return !(si == 4095); }
+int neq_4096(signed int si) { return !(si == 4096); }
+int neq_4097(signed int si) { return !(si == 4097); }
+int neq_8191(signed int si) { return !(si == 8191); }
+int neq_8192(signed int si) { return !(si == 8192); }
+int neq_8193(signed int si) { return !(si == 8193); }
+int neq_16383(signed int si) { return !(si == 16383); }
+int neq_16384(signed int si) { return !(si == 16384); }
+int neq_16385(signed int si) { return !(si == 16385); }
+int neq_32767(signed int si) { return !(si == 32767); }
+int neq_32768(signed int si) { return !(si == 32768); }
+int neq_32769(signed int si) { return !(si == 32769); }
+int neq_65534(signed int si) { return !(si == 65534); }
+int neq_65535(signed int si) { return !(si == 65535); }
+int neq_65536(signed int si) { return !(si == 65536); }
+int neq_65537(signed int si) { return !(si == 65537); }
+int neq_131071(signed int si) { return !(si == 131071); }
+int neq_131072(signed int si) { return !(si == 131072); }
+int neq_131073(signed int si) { return !(si == 131073); }
+int neq_262143(signed int si) { return !(si == 262143); }
+int neq_262144(signed int si) { return !(si == 262144); }
+int neq_262145(signed int si) { return !(si == 262145); }
+int neq_524287(signed int si) { return !(si == 524287); }
+int neq_524288(signed int si) { return !(si == 524288); }
+int neq_524289(signed int si) { return !(si == 524289); }
+int neq_1048575(signed int si) { return !(si == 1048575); }
+int neq_1048576(signed int si) { return !(si == 1048576); }
+int neq_1048577(signed int si) { return !(si == 1048577); }
+int neq_2097151(signed int si) { return !(si == 2097151); }
+int neq_2097152(signed int si) { return !(si == 2097152); }
+int neq_2097153(signed int si) { return !(si == 2097153); }
+int neq_4194303(signed int si) { return !(si == 4194303); }
+int neq_4194304(signed int si) { return !(si == 4194304); }
+int neq_4194305(signed int si) { return !(si == 4194305); }
+int neq_8388607(signed int si) { return !(si == 8388607); }
+int neq_8388608(signed int si) { return !(si == 8388608); }
+int neq_8388609(signed int si) { return !(si == 8388609); }
+int neq_16777215(signed int si) { return !(si == 16777215); }
+int neq_16777216(signed int si) { return !(si == 16777216); }
+int neq_16777217(signed int si) { return !(si == 16777217); }
+int neq_33554431(signed int si) { return !(si == 33554431); }
+int neq_33554432(signed int si) { return !(si == 33554432); }
+int neq_33554433(signed int si) { return !(si == 33554433); }
+int neq_67108863(signed int si) { return !(si == 67108863); }
+int neq_67108864(signed int si) { return !(si == 67108864); }
+int neq_67108865(signed int si) { return !(si == 67108865); }
+int neq_134217727(signed int si) { return !(si == 134217727); }
+int neq_134217728(signed int si) { return !(si == 134217728); }
+int neq_134217729(signed int si) { return !(si == 134217729); }
+int neq_268435455(signed int si) { return !(si == 268435455); }
+int neq_268435456(signed int si) { return !(si == 268435456); }
+int neq_268435457(signed int si) { return !(si == 268435457); }
+int neq_536870911(signed int si) { return !(si == 536870911); }
+int neq_536870912(signed int si) { return !(si == 536870912); }
+int neq_536870913(signed int si) { return !(si == 536870913); }
+int neq_1073741823(signed int si) { return !(si == 1073741823); }
+int neq_1073741824(signed int si) { return !(si == 1073741824); }
+int neq_1073741825(signed int si) { return !(si == 1073741825); }
+
+static signed int si;
+int main() {
+
+  si = -1073741824;
+  my_assert(neq_m1073741825(si) == 1);
+  my_assert(neq_m1073741824(si) != 1);
+  my_assert(neq_m1073741823(si) == 1);
+
+  si = -536870912;
+  my_assert(neq_m536870913(si) == 1);
+  my_assert(neq_m536870912(si) != 1);
+  my_assert(neq_m536870911(si) == 1);
+
+  si = -268435456;
+  my_assert(neq_m268435457(si) == 1);
+  my_assert(neq_m268435456(si) != 1);
+  my_assert(neq_m268435455(si) == 1);
+
+  si = -134217728;
+  my_assert(neq_m134217729(si) == 1);
+  my_assert(neq_m134217728(si) != 1);
+  my_assert(neq_m134217727(si) == 1);
+
+  si = -67108864;
+  my_assert(neq_m67108865(si) == 1);
+  my_assert(neq_m67108864(si) != 1);
+  my_assert(neq_m67108863(si) == 1);
+
+  si = -33554432;
+  my_assert(neq_m33554433(si) == 1);
+  my_assert(neq_m33554432(si) != 1);
+  my_assert(neq_m33554431(si) == 1);
+
+  si = -16777216;
+  my_assert(neq_m16777217(si) == 1);
+  my_assert(neq_m16777216(si) != 1);
+  my_assert(neq_m16777215(si) == 1);
+
+  si = -8388608;
+  my_assert(neq_m8388609(si) == 1);
+  my_assert(neq_m8388608(si) != 1);
+  my_assert(neq_m8388607(si) == 1);
+
+  si = -4194304;
+  my_assert(neq_m4194305(si) == 1);
+  my_assert(neq_m4194304(si) != 1);
+  my_assert(neq_m4194303(si) == 1);
+
+  si = -2097152;
+  my_assert(neq_m2097153(si) == 1);
+  my_assert(neq_m2097152(si) != 1);
+  my_assert(neq_m2097151(si) == 1);
+
+  si = -1048576;
+  my_assert(neq_m1048577(si) == 1);
+  my_assert(neq_m1048576(si) != 1);
+  my_assert(neq_m1048575(si) == 1);
+
+  si = -524288;
+  my_assert(neq_m524289(si) == 1);
+  my_assert(neq_m524288(si) != 1);
+  my_assert(neq_m524287(si) == 1);
+
+  si = -262144;
+  my_assert(neq_m262145(si) == 1);
+  my_assert(neq_m262144(si) != 1);
+  my_assert(neq_m262143(si) == 1);
+
+  si = -131072;
+  my_assert(neq_m131073(si) == 1);
+  my_assert(neq_m131072(si) != 1);
+  my_assert(neq_m131071(si) == 1);
+
+  si = -65535;
+  my_assert(neq_m65536(si) == 1);
+  my_assert(neq_m65535(si) != 1);
+  my_assert(neq_m65534(si) == 1);
+
+  si = -32768;
+  my_assert(neq_m32769(si) == 1);
+  my_assert(neq_m32768(si) != 1);
+  my_assert(neq_m32767(si) == 1);
+
+  si = -16384;
+  my_assert(neq_m16385(si) == 1);
+  my_assert(neq_m16384(si) != 1);
+  my_assert(neq_m16383(si) == 1);
+
+  si = -8192;
+  my_assert(neq_m8193(si) == 1);
+  my_assert(neq_m8192(si) != 1);
+  my_assert(neq_m8191(si) == 1);
+
+  si = -4096;
+  my_assert(neq_m4097(si) == 1);
+  my_assert(neq_m4096(si) != 1);
+  my_assert(neq_m4095(si) == 1);
+
+  si = -2048;
+  my_assert(neq_m2049(si) == 1);
+  my_assert(neq_m2048(si) != 1);
+  my_assert(neq_m2047(si) == 1);
+
+  si = -1024;
+  my_assert(neq_m1025(si) == 1);
+  my_assert(neq_m1024(si) != 1);
+  my_assert(neq_m1023(si) == 1);
+
+  si = -512;
+  my_assert(neq_m513(si) == 1);
+  my_assert(neq_m512(si) != 1);
+  my_assert(neq_m511(si) == 1);
+
+  si = -256;
+  my_assert(neq_m257(si) == 1);
+  my_assert(neq_m256(si) != 1);
+  my_assert(neq_m255(si) == 1);
+
+  si = -128;
+  my_assert(neq_m129(si) == 1);
+  my_assert(neq_m128(si) != 1);
+  my_assert(neq_m127(si) == 1);
+
+  si = -64;
+  my_assert(neq_m65(si) == 1);
+  my_assert(neq_m64(si) != 1);
+  my_assert(neq_m63(si) == 1);
+
+  si = -32;
+  my_assert(neq_m33(si) == 1);
+  my_assert(neq_m32(si) != 1);
+  my_assert(neq_m31(si) == 1);
+
+  si = -16;
+  my_assert(neq_m17(si) == 1);
+  my_assert(neq_m16(si) != 1);
+  my_assert(neq_m15(si) == 1);
+
+  si = -8;
+  my_assert(neq_m9(si) == 1);
+  my_assert(neq_m8(si) != 1);
+  my_assert(neq_m7(si) == 1);
+
+  si = -4;
+  my_assert(neq_m5(si) == 1);
+  my_assert(neq_m4(si) != 1);
+  my_assert(neq_m3(si) == 1);
+
+  si = -2;
+  my_assert(neq_m3(si) == 1);
+  my_assert(neq_m2(si) != 1);
+  my_assert(neq_m1(si) == 1);
+
+  si = -1;
+  my_assert(neq_m2(si) == 1);
+  my_assert(neq_m1(si) != 1);
+  my_assert(neq_00(si) == 1);
+
+  si = 0;
+  my_assert(neq_m1(si) == 1);
+  my_assert(neq_00(si) != 1);
+  my_assert(neq_01(si) == 1);
+
+  si = 1;
+  my_assert(neq_00(si) == 1);
+  my_assert(neq_01(si) != 1);
+  my_assert(neq_02(si) == 1);
+
+  si = 2;
+  my_assert(neq_01(si) == 1);
+  my_assert(neq_02(si) != 1);
+  my_assert(neq_03(si) == 1);
+
+  si = 4;
+  my_assert(neq_03(si) == 1);
+  my_assert(neq_04(si) != 1);
+  my_assert(neq_05(si) == 1);
+
+  si = 8;
+  my_assert(neq_07(si) == 1);
+  my_assert(neq_08(si) != 1);
+  my_assert(neq_09(si) == 1);
+
+  si = 16;
+  my_assert(neq_15(si) == 1);
+  my_assert(neq_16(si) != 1);
+  my_assert(neq_17(si) == 1);
+
+  si = 32;
+  my_assert(neq_31(si) == 1);
+  my_assert(neq_32(si) != 1);
+  my_assert(neq_33(si) == 1);
+
+  si = 64;
+  my_assert(neq_63(si) == 1);
+  my_assert(neq_64(si) != 1);
+  my_assert(neq_65(si) == 1);
+
+  si = 128;
+  my_assert(neq_127(si) == 1);
+  my_assert(neq_128(si) != 1);
+  my_assert(neq_129(si) == 1);
+
+  si = 256;
+  my_assert(neq_255(si) == 1);
+  my_assert(neq_256(si) != 1);
+  my_assert(neq_257(si) == 1);
+
+  si = 512;
+  my_assert(neq_511(si) == 1);
+  my_assert(neq_512(si) != 1);
+  my_assert(neq_513(si) == 1);
+
+  si = 1024;
+  my_assert(neq_1023(si) == 1);
+  my_assert(neq_1024(si) != 1);
+  my_assert(neq_1025(si) == 1);
+
+  si = 2048;
+  my_assert(neq_2047(si) == 1);
+  my_assert(neq_2048(si) != 1);
+  my_assert(neq_2049(si) == 1);
+
+  si = 4096;
+  my_assert(neq_4095(si) == 1);
+  my_assert(neq_4096(si) != 1);
+  my_assert(neq_4097(si) == 1);
+
+  si = 8192;
+  my_assert(neq_8191(si) == 1);
+  my_assert(neq_8192(si) != 1);
+  my_assert(neq_8193(si) == 1);
+
+  si = 16384;
+  my_assert(neq_16383(si) == 1);
+  my_assert(neq_16384(si) != 1);
+  my_assert(neq_16385(si) == 1);
+
+  si = 32768;
+  my_assert(neq_32767(si) == 1);
+  my_assert(neq_32768(si) != 1);
+  my_assert(neq_32769(si) == 1);
+
+  si = 65536;
+  my_assert(neq_65535(si) == 1);
+  my_assert(neq_65536(si) != 1);
+  my_assert(neq_65537(si) == 1);
+
+  si = 131072;
+  my_assert(neq_131071(si) == 1);
+  my_assert(neq_131072(si) != 1);
+  my_assert(neq_131073(si) == 1);
+
+  si = 262144;
+  my_assert(neq_262143(si) == 1);
+  my_assert(neq_262144(si) != 1);
+  my_assert(neq_262145(si) == 1);
+
+  si = 524288;
+  my_assert(neq_524287(si) == 1);
+  my_assert(neq_524288(si) != 1);
+  my_assert(neq_524289(si) == 1);
+
+  si = 1048576;
+  my_assert(neq_1048575(si) == 1);
+  my_assert(neq_1048576(si) != 1);
+  my_assert(neq_1048577(si) == 1);
+
+  si = 2097152;
+  my_assert(neq_2097151(si) == 1);
+  my_assert(neq_2097152(si) != 1);
+  my_assert(neq_2097153(si) == 1);
+
+  si = 4194304;
+  my_assert(neq_4194303(si) == 1);
+  my_assert(neq_4194304(si) != 1);
+  my_assert(neq_4194305(si) == 1);
+
+  si = 8388608;
+  my_assert(neq_8388607(si) == 1);
+  my_assert(neq_8388608(si) != 1);
+  my_assert(neq_8388609(si) == 1);
+
+  si = 16777216;
+  my_assert(neq_16777215(si) == 1);
+  my_assert(neq_16777216(si) != 1);
+  my_assert(neq_16777217(si) == 1);
+
+  si = 33554432;
+  my_assert(neq_33554431(si) == 1);
+  my_assert(neq_33554432(si) != 1);
+  my_assert(neq_33554433(si) == 1);
+
+  si = 67108864;
+  my_assert(neq_67108863(si) == 1);
+  my_assert(neq_67108864(si) != 1);
+  my_assert(neq_67108865(si) == 1);
+
+  si = 134217728;
+  my_assert(neq_134217727(si) == 1);
+  my_assert(neq_134217728(si) != 1);
+  my_assert(neq_134217729(si) == 1);
+
+  si = 268435456;
+  my_assert(neq_268435455(si) == 1);
+  my_assert(neq_268435456(si) != 1);
+  my_assert(neq_268435457(si) == 1);
+
+  si = 536870912;
+  my_assert(neq_536870911(si) == 1);
+  my_assert(neq_536870912(si) != 1);
+  my_assert(neq_536870913(si) == 1);
+
+  si = 1073741824;
+  my_assert(neq_1073741823(si) == 1);
+  my_assert(neq_1073741824(si) != 1);
+  my_assert(neq_1073741825(si) == 1);
+
+  return exit_status;
+}
