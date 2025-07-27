1a6cde7cf40266e1194d2d16857f49c7f8ff8a80
xyzzy <xyzzy@xyzzys-iMac.local>
2025-07-27 09:50:32 -0500

    s_gt_jmp

---
+++ runnable/s_gt_jmp.c
@@ -0,0 +1,689 @@
+
+
+int exit_status;
+
+#include "my_assert.h"
+
+int gt_m32768(signed short ss) {
+  if (ss > -32768)
+    return 1;
+  else
+    return 0;
+}
+int gt_m32767(signed short ss) {
+  if (ss > -32767)
+    return 1;
+  else
+    return 0;
+}
+int gt_m16385(signed short ss) {
+  if (ss > -16385)
+    return 1;
+  else
+    return 0;
+}
+int gt_m16384(signed short ss) {
+  if (ss > -16384)
+    return 1;
+  else
+    return 0;
+}
+int gt_m16383(signed short ss) {
+  if (ss > -16383)
+    return 1;
+  else
+    return 0;
+}
+int gt_m8193(signed short ss) {
+  if (ss > -8193)
+    return 1;
+  else
+    return 0;
+}
+int gt_m8192(signed short ss) {
+  if (ss > -8192)
+    return 1;
+  else
+    return 0;
+}
+int gt_m8191(signed short ss) {
+  if (ss > -8191)
+    return 1;
+  else
+    return 0;
+}
+int gt_m4097(signed short ss) {
+  if (ss > -4097)
+    return 1;
+  else
+    return 0;
+}
+int gt_m4096(signed short ss) {
+  if (ss > -4096)
+    return 1;
+  else
+    return 0;
+}
+int gt_m4095(signed short ss) {
+  if (ss > -4095)
+    return 1;
+  else
+    return 0;
+}
+int gt_m2049(signed short ss) {
+  if (ss > -2049)
+    return 1;
+  else
+    return 0;
+}
+int gt_m2048(signed short ss) {
+  if (ss > -2048)
+    return 1;
+  else
+    return 0;
+}
+int gt_m2047(signed short ss) {
+  if (ss > -2047)
+    return 1;
+  else
+    return 0;
+}
+int gt_m1025(signed short ss) {
+  if (ss > -1025)
+    return 1;
+  else
+    return 0;
+}
+int gt_m1024(signed short ss) {
+  if (ss > -1024)
+    return 1;
+  else
+    return 0;
+}
+int gt_m1023(signed short ss) {
+  if (ss > -1023)
+    return 1;
+  else
+    return 0;
+}
+int gt_m513(signed short ss) {
+  if (ss > -513)
+    return 1;
+  else
+    return 0;
+}
+int gt_m512(signed short ss) {
+  if (ss > -512)
+    return 1;
+  else
+    return 0;
+}
+int gt_m511(signed short ss) {
+  if (ss > -511)
+    return 1;
+  else
+    return 0;
+}
+int gt_m257(signed short ss) {
+  if (ss > -257)
+    return 1;
+  else
+    return 0;
+}
+int gt_m256(signed short ss) {
+  if (ss > -256)
+    return 1;
+  else
+    return 0;
+}
+int gt_m255(signed short ss) {
+  if (ss > -255)
+    return 1;
+  else
+    return 0;
+}
+int gt_m129(signed short ss) {
+  if (ss > -129)
+    return 1;
+  else
+    return 0;
+}
+int gt_m128(signed short ss) {
+  if (ss > -128)
+    return 1;
+  else
+    return 0;
+}
+int gt_m127(signed short ss) {
+  if (ss > -127)
+    return 1;
+  else
+    return 0;
+}
+int gt_m65(signed short ss) {
+  if (ss > -65)
+    return 1;
+  else
+    return 0;
+}
+int gt_m64(signed short ss) {
+  if (ss > -64)
+    return 1;
+  else
+    return 0;
+}
+int gt_m63(signed short ss) {
+  if (ss > -63)
+    return 1;
+  else
+    return 0;
+}
+int gt_m33(signed short ss) {
+  if (ss > -33)
+    return 1;
+  else
+    return 0;
+}
+int gt_m32(signed short ss) {
+  if (ss > -32)
+    return 1;
+  else
+    return 0;
+}
+int gt_m31(signed short ss) {
+  if (ss > -31)
+    return 1;
+  else
+    return 0;
+}
+int gt_m17(signed short ss) {
+  if (ss > -17)
+    return 1;
+  else
+    return 0;
+}
+int gt_m16(signed short ss) {
+  if (ss > -16)
+    return 1;
+  else
+    return 0;
+}
+int gt_m15(signed short ss) {
+  if (ss > -15)
+    return 1;
+  else
+    return 0;
+}
+int gt_m9(signed short ss) {
+  if (ss > -9)
+    return 1;
+  else
+    return 0;
+}
+int gt_m8(signed short ss) {
+  if (ss > -8)
+    return 1;
+  else
+    return 0;
+}
+int gt_m7(signed short ss) {
+  if (ss > -7)
+    return 1;
+  else
+    return 0;
+}
+int gt_m5(signed short ss) {
+  if (ss > -5)
+    return 1;
+  else
+    return 0;
+}
+int gt_m4(signed short ss) {
+  if (ss > -4)
+    return 1;
+  else
+    return 0;
+}
+int gt_m3(signed short ss) {
+  if (ss > -3)
+    return 1;
+  else
+    return 0;
+}
+int gt_m2(signed short ss) {
+  if (ss > -2)
+    return 1;
+  else
+    return 0;
+}
+int gt_m1(signed short ss) {
+  if (ss > -1)
+    return 1;
+  else
+    return 0;
+}
+int gt_00(signed short ss) {
+  if (ss > 0)
+    return 1;
+  else
+    return 0;
+}
+int gt_01(signed short ss) {
+  if (ss > 1)
+    return 1;
+  else
+    return 0;
+}
+int gt_02(signed short ss) {
+  if (ss > 2)
+    return 1;
+  else
+    return 0;
+}
+int gt_03(signed short ss) {
+  if (ss > 3)
+    return 1;
+  else
+    return 0;
+}
+int gt_04(signed short ss) {
+  if (ss > 4)
+    return 1;
+  else
+    return 0;
+}
+int gt_05(signed short ss) {
+  if (ss > 5)
+    return 1;
+  else
+    return 0;
+}
+int gt_07(signed short ss) {
+  if (ss > 7)
+    return 1;
+  else
+    return 0;
+}
+int gt_08(signed short ss) {
+  if (ss > 8)
+    return 1;
+  else
+    return 0;
+}
+int gt_09(signed short ss) {
+  if (ss > 9)
+    return 1;
+  else
+    return 0;
+}
+int gt_15(signed short ss) {
+  if (ss > 15)
+    return 1;
+  else
+    return 0;
+}
+int gt_16(signed short ss) {
+  if (ss > 16)
+    return 1;
+  else
+    return 0;
+}
+int gt_17(signed short ss) {
+  if (ss > 17)
+    return 1;
+  else
+    return 0;
+}
+int gt_31(signed short ss) {
+  if (ss > 31)
+    return 1;
+  else
+    return 0;
+}
+int gt_32(signed short ss) {
+  if (ss > 32)
+    return 1;
+  else
+    return 0;
+}
+int gt_33(signed short ss) {
+  if (ss > 33)
+    return 1;
+  else
+    return 0;
+}
+int gt_63(signed short ss) {
+  if (ss > 63)
+    return 1;
+  else
+    return 0;
+}
+int gt_64(signed short ss) {
+  if (ss > 64)
+    return 1;
+  else
+    return 0;
+}
+int gt_65(signed short ss) {
+  if (ss > 65)
+    return 1;
+  else
+    return 0;
+}
+int gt_127(signed short ss) {
+  if (ss > 127)
+    return 1;
+  else
+    return 0;
+}
+int gt_128(signed short ss) {
+  if (ss > 128)
+    return 1;
+  else
+    return 0;
+}
+int gt_129(signed short ss) {
+  if (ss > 129)
+    return 1;
+  else
+    return 0;
+}
+int gt_255(signed short ss) {
+  if (ss > 255)
+    return 1;
+  else
+    return 0;
+}
+int gt_256(signed short ss) {
+  if (ss > 256)
+    return 1;
+  else
+    return 0;
+}
+int gt_257(signed short ss) {
+  if (ss > 257)
+    return 1;
+  else
+    return 0;
+}
+int gt_511(signed short ss) {
+  if (ss > 511)
+    return 1;
+  else
+    return 0;
+}
+int gt_512(signed short ss) {
+  if (ss > 512)
+    return 1;
+  else
+    return 0;
+}
+int gt_513(signed short ss) {
+  if (ss > 513)
+    return 1;
+  else
+    return 0;
+}
+int gt_1023(signed short ss) {
+  if (ss > 1023)
+    return 1;
+  else
+    return 0;
+}
+int gt_1024(signed short ss) {
+  if (ss > 1024)
+    return 1;
+  else
+    return 0;
+}
+int gt_1025(signed short ss) {
+  if (ss > 1025)
+    return 1;
+  else
+    return 0;
+}
+int gt_2047(signed short ss) {
+  if (ss > 2047)
+    return 1;
+  else
+    return 0;
+}
+int gt_2048(signed short ss) {
+  if (ss > 2048)
+    return 1;
+  else
+    return 0;
+}
+int gt_2049(signed short ss) {
+  if (ss > 2049)
+    return 1;
+  else
+    return 0;
+}
+int gt_4095(signed short ss) {
+  if (ss > 4095)
+    return 1;
+  else
+    return 0;
+}
+int gt_4096(signed short ss) {
+  if (ss > 4096)
+    return 1;
+  else
+    return 0;
+}
+int gt_4097(signed short ss) {
+  if (ss > 4097)
+    return 1;
+  else
+    return 0;
+}
+int gt_8191(signed short ss) {
+  if (ss > 8191)
+    return 1;
+  else
+    return 0;
+}
+int gt_8192(signed short ss) {
+  if (ss > 8192)
+    return 1;
+  else
+    return 0;
+}
+int gt_8193(signed short ss) {
+  if (ss > 8193)
+    return 1;
+  else
+    return 0;
+}
+int gt_16383(signed short ss) {
+  if (ss > 16383)
+    return 1;
+  else
+    return 0;
+}
+int gt_16384(signed short ss) {
+  if (ss > 16384)
+    return 1;
+  else
+    return 0;
+}
+int gt_16385(signed short ss) {
+  if (ss > 16385)
+    return 1;
+  else
+    return 0;
+}
+int gt_32767(signed short ss) {
+  if (ss > 32767)
+    return 1;
+  else
+    return 0;
+}
+
+static signed short ss;
+int main() {
+  ss = -32768;
+  my_assert(gt_m32768(ss) != 1);
+  my_assert(gt_m32767(ss) != 1);
+
+  ss = -16384;
+  my_assert(gt_m16385(ss) == 1);
+  my_assert(gt_m16384(ss) != 1);
+  my_assert(gt_m16383(ss) != 1);
+
+  ss = -8192;
+  my_assert(gt_m8193(ss) == 1);
+  my_assert(gt_m8192(ss) != 1);
+  my_assert(gt_m8191(ss) != 1);
+
+  ss = -4096;
+  my_assert(gt_m4097(ss) == 1);
+  my_assert(gt_m4096(ss) != 1);
+  my_assert(gt_m4095(ss) != 1);
+
+  ss = -2048;
+  my_assert(gt_m2049(ss) == 1);
+  my_assert(gt_m2048(ss) != 1);
+  my_assert(gt_m2047(ss) != 1);
+
+  ss = -1024;
+  my_assert(gt_m1025(ss) == 1);
+  my_assert(gt_m1024(ss) != 1);
+  my_assert(gt_m1023(ss) != 1);
+
+  ss = -512;
+  my_assert(gt_m513(ss) == 1);
+  my_assert(gt_m512(ss) != 1);
+  my_assert(gt_m511(ss) != 1);
+
+  ss = -256;
+  my_assert(gt_m257(ss) == 1);
+  my_assert(gt_m256(ss) != 1);
+  my_assert(gt_m255(ss) != 1);
+
+  ss = -128;
+  my_assert(gt_m129(ss) == 1);
+  my_assert(gt_m128(ss) != 1);
+  my_assert(gt_m127(ss) != 1);
+
+  ss = -64;
+  my_assert(gt_m65(ss) == 1);
+  my_assert(gt_m64(ss) != 1);
+  my_assert(gt_m63(ss) != 1);
+
+  ss = -32;
+  my_assert(gt_m33(ss) == 1);
+  my_assert(gt_m32(ss) != 1);
+  my_assert(gt_m31(ss) != 1);
+
+  ss = -16;
+  my_assert(gt_m17(ss) == 1);
+  my_assert(gt_m16(ss) != 1);
+  my_assert(gt_m15(ss) != 1);
+
+  ss = -8;
+  my_assert(gt_m9(ss) == 1);
+  my_assert(gt_m8(ss) != 1);
+  my_assert(gt_m7(ss) != 1);
+
+  ss = -4;
+  my_assert(gt_m5(ss) == 1);
+  my_assert(gt_m4(ss) != 1);
+  my_assert(gt_m3(ss) != 1);
+
+  ss = -2;
+  my_assert(gt_m3(ss) == 1);
+  my_assert(gt_m2(ss) != 1);
+  my_assert(gt_m1(ss) != 1);
+
+  ss = -1;
+  my_assert(gt_m2(ss) == 1);
+  my_assert(gt_m1(ss) != 1);
+  my_assert(gt_00(ss) != 1);
+
+  ss = 0;
+  my_assert(gt_m1(ss) == 1);
+  my_assert(gt_00(ss) != 1);
+  my_assert(gt_01(ss) != 1);
+
+  ss = 1;
+  my_assert(gt_00(ss) == 1);
+  my_assert(gt_01(ss) != 1);
+  my_assert(gt_02(ss) != 1);
+
+  ss = 2;
+  my_assert(gt_01(ss) == 1);
+  my_assert(gt_02(ss) != 1);
+  my_assert(gt_03(ss) != 1);
+
+  ss = 4;
+  my_assert(gt_03(ss) == 1);
+  my_assert(gt_04(ss) != 1);
+  my_assert(gt_05(ss) != 1);
+
+  ss = 8;
+  my_assert(gt_07(ss) == 1);
+  my_assert(gt_08(ss) != 1);
+  my_assert(gt_09(ss) != 1);
+
+  ss = 16;
+  my_assert(gt_15(ss) == 1);
+  my_assert(gt_16(ss) != 1);
+  my_assert(gt_17(ss) != 1);
+
+  ss = 32;
+  my_assert(gt_31(ss) == 1);
+  my_assert(gt_32(ss) != 1);
+  my_assert(gt_33(ss) != 1);
+
+  ss = 64;
+  my_assert(gt_63(ss) == 1);
+  my_assert(gt_64(ss) != 1);
+  my_assert(gt_65(ss) != 1);
+
+  ss = 128;
+  my_assert(gt_127(ss) == 1);
+  my_assert(gt_128(ss) != 1);
+  my_assert(gt_129(ss) != 1);
+
+  ss = 256;
+  my_assert(gt_255(ss) == 1);
+  my_assert(gt_256(ss) != 1);
+  my_assert(gt_257(ss) != 1);
+
+  ss = 512;
+  my_assert(gt_511(ss) == 1);
+  my_assert(gt_512(ss) != 1);
+  my_assert(gt_513(ss) != 1);
+
+  ss = 1024;
+  my_assert(gt_1023(ss) == 1);
+  my_assert(gt_1024(ss) != 1);
+  my_assert(gt_1025(ss) != 1);
+
+  ss = 2048;
+  my_assert(gt_2047(ss) == 1);
+  my_assert(gt_2048(ss) != 1);
+  my_assert(gt_2049(ss) != 1);
+
+  ss = 4096;
+  my_assert(gt_4095(ss) == 1);
+  my_assert(gt_4096(ss) != 1);
+  my_assert(gt_4097(ss) != 1);
+
+  ss = 8192;
+  my_assert(gt_8191(ss) == 1);
+  my_assert(gt_8192(ss) != 1);
+  my_assert(gt_8193(ss) != 1);
+
+  ss = 16384;
+  my_assert(gt_16383(ss) == 1);
+  my_assert(gt_16384(ss) != 1);
+  my_assert(gt_16385(ss) != 1);
+
+  ss = 32767;
+  my_assert(gt_32767(ss) != 1);
+
+  return exit_status;
+}
