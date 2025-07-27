5dcd7284189d13b747b697d0193d58bb485613f1
xyzzy <xyzzy@xyzzys-iMac.local>
2025-07-27 09:33:49 -0500

    l_gt

---
+++ runnable/l_gt.c
@@ -0,0 +1,507 @@
+
+
+int exit_status;
+
+#include "my_assert.h"
+
+int gt_m1073741825(signed long sl) { return sl > -1073741825; }
+int gt_m1073741824(signed long sl) { return sl > -1073741824; }
+int gt_m1073741823(signed long sl) { return sl > -1073741823; }
+int gt_m536870913(signed long sl) { return sl > -536870913; }
+int gt_m536870912(signed long sl) { return sl > -536870912; }
+int gt_m536870911(signed long sl) { return sl > -536870911; }
+int gt_m268435457(signed long sl) { return sl > -268435457; }
+int gt_m268435456(signed long sl) { return sl > -268435456; }
+int gt_m268435455(signed long sl) { return sl > -268435455; }
+int gt_m134217729(signed long sl) { return sl > -134217729; }
+int gt_m134217728(signed long sl) { return sl > -134217728; }
+int gt_m134217727(signed long sl) { return sl > -134217727; }
+int gt_m67108865(signed long sl) { return sl > -67108865; }
+int gt_m67108864(signed long sl) { return sl > -67108864; }
+int gt_m67108863(signed long sl) { return sl > -67108863; }
+int gt_m33554433(signed long sl) { return sl > -33554433; }
+int gt_m33554432(signed long sl) { return sl > -33554432; }
+int gt_m33554431(signed long sl) { return sl > -33554431; }
+int gt_m16777217(signed long sl) { return sl > -16777217; }
+int gt_m16777216(signed long sl) { return sl > -16777216; }
+int gt_m16777215(signed long sl) { return sl > -16777215; }
+int gt_m8388609(signed long sl) { return sl > -8388609; }
+int gt_m8388608(signed long sl) { return sl > -8388608; }
+int gt_m8388607(signed long sl) { return sl > -8388607; }
+int gt_m4194305(signed long sl) { return sl > -4194305; }
+int gt_m4194304(signed long sl) { return sl > -4194304; }
+int gt_m4194303(signed long sl) { return sl > -4194303; }
+int gt_m2097153(signed long sl) { return sl > -2097153; }
+int gt_m2097152(signed long sl) { return sl > -2097152; }
+int gt_m2097151(signed long sl) { return sl > -2097151; }
+int gt_m1048577(signed long sl) { return sl > -1048577; }
+int gt_m1048576(signed long sl) { return sl > -1048576; }
+int gt_m1048575(signed long sl) { return sl > -1048575; }
+int gt_m524289(signed long sl) { return sl > -524289; }
+int gt_m524288(signed long sl) { return sl > -524288; }
+int gt_m524287(signed long sl) { return sl > -524287; }
+int gt_m262145(signed long sl) { return sl > -262145; }
+int gt_m262144(signed long sl) { return sl > -262144; }
+int gt_m262143(signed long sl) { return sl > -262143; }
+int gt_m131073(signed long sl) { return sl > -131073; }
+int gt_m131072(signed long sl) { return sl > -131072; }
+int gt_m131071(signed long sl) { return sl > -131071; }
+int gt_m65536(signed long sl) { return sl > -65536; }
+int gt_m65535(signed long sl) { return sl > -65535; }
+int gt_m65534(signed long sl) { return sl > -65534; }
+int gt_m32769(signed long sl) { return sl > -32769; }
+int gt_m32768(signed long sl) { return sl > -32768; }
+int gt_m32767(signed long sl) { return sl > -32767; }
+int gt_m16385(signed long sl) { return sl > -16385; }
+int gt_m16384(signed long sl) { return sl > -16384; }
+int gt_m16383(signed long sl) { return sl > -16383; }
+int gt_m8193(signed long sl) { return sl > -8193; }
+int gt_m8192(signed long sl) { return sl > -8192; }
+int gt_m8191(signed long sl) { return sl > -8191; }
+int gt_m4097(signed long sl) { return sl > -4097; }
+int gt_m4096(signed long sl) { return sl > -4096; }
+int gt_m4095(signed long sl) { return sl > -4095; }
+int gt_m2049(signed long sl) { return sl > -2049; }
+int gt_m2048(signed long sl) { return sl > -2048; }
+int gt_m2047(signed long sl) { return sl > -2047; }
+int gt_m1025(signed long sl) { return sl > -1025; }
+int gt_m1024(signed long sl) { return sl > -1024; }
+int gt_m1023(signed long sl) { return sl > -1023; }
+int gt_m513(signed long sl) { return sl > -513; }
+int gt_m512(signed long sl) { return sl > -512; }
+int gt_m511(signed long sl) { return sl > -511; }
+int gt_m257(signed long sl) { return sl > -257; }
+int gt_m256(signed long sl) { return sl > -256; }
+int gt_m255(signed long sl) { return sl > -255; }
+int gt_m129(signed long sl) { return sl > -129; }
+int gt_m128(signed long sl) { return sl > -128; }
+int gt_m127(signed long sl) { return sl > -127; }
+int gt_m65(signed long sl) { return sl > -65; }
+int gt_m64(signed long sl) { return sl > -64; }
+int gt_m63(signed long sl) { return sl > -63; }
+int gt_m33(signed long sl) { return sl > -33; }
+int gt_m32(signed long sl) { return sl > -32; }
+int gt_m31(signed long sl) { return sl > -31; }
+int gt_m17(signed long sl) { return sl > -17; }
+int gt_m16(signed long sl) { return sl > -16; }
+int gt_m15(signed long sl) { return sl > -15; }
+int gt_m9(signed long sl) { return sl > -9; }
+int gt_m8(signed long sl) { return sl > -8; }
+int gt_m7(signed long sl) { return sl > -7; }
+int gt_m5(signed long sl) { return sl > -5; }
+int gt_m4(signed long sl) { return sl > -4; }
+int gt_m3(signed long sl) { return sl > -3; }
+int gt_m2(signed long sl) { return sl > -2; }
+int gt_m1(signed long sl) { return sl > -1; }
+int gt_00(signed long sl) { return sl > 0; }
+int gt_01(signed long sl) { return sl > 1; }
+int gt_02(signed long sl) { return sl > 2; }
+int gt_03(signed long sl) { return sl > 3; }
+int gt_04(signed long sl) { return sl > 4; }
+int gt_05(signed long sl) { return sl > 5; }
+int gt_07(signed long sl) { return sl > 7; }
+int gt_08(signed long sl) { return sl > 8; }
+int gt_09(signed long sl) { return sl > 9; }
+int gt_15(signed long sl) { return sl > 15; }
+int gt_16(signed long sl) { return sl > 16; }
+int gt_17(signed long sl) { return sl > 17; }
+int gt_31(signed long sl) { return sl > 31; }
+int gt_32(signed long sl) { return sl > 32; }
+int gt_33(signed long sl) { return sl > 33; }
+int gt_63(signed long sl) { return sl > 63; }
+int gt_64(signed long sl) { return sl > 64; }
+int gt_65(signed long sl) { return sl > 65; }
+int gt_127(signed long sl) { return sl > 127; }
+int gt_128(signed long sl) { return sl > 128; }
+int gt_129(signed long sl) { return sl > 129; }
+int gt_255(signed long sl) { return sl > 255; }
+int gt_256(signed long sl) { return sl > 256; }
+int gt_257(signed long sl) { return sl > 257; }
+int gt_511(signed long sl) { return sl > 511; }
+int gt_512(signed long sl) { return sl > 512; }
+int gt_513(signed long sl) { return sl > 513; }
+int gt_1023(signed long sl) { return sl > 1023; }
+int gt_1024(signed long sl) { return sl > 1024; }
+int gt_1025(signed long sl) { return sl > 1025; }
+int gt_2047(signed long sl) { return sl > 2047; }
+int gt_2048(signed long sl) { return sl > 2048; }
+int gt_2049(signed long sl) { return sl > 2049; }
+int gt_4095(signed long sl) { return sl > 4095; }
+int gt_4096(signed long sl) { return sl > 4096; }
+int gt_4097(signed long sl) { return sl > 4097; }
+int gt_8191(signed long sl) { return sl > 8191; }
+int gt_8192(signed long sl) { return sl > 8192; }
+int gt_8193(signed long sl) { return sl > 8193; }
+int gt_16383(signed long sl) { return sl > 16383; }
+int gt_16384(signed long sl) { return sl > 16384; }
+int gt_16385(signed long sl) { return sl > 16385; }
+int gt_32767(signed long sl) { return sl > 32767; }
+int gt_32768(signed long sl) { return sl > 32768; }
+int gt_32769(signed long sl) { return sl > 32769; }
+int gt_65534(signed long sl) { return sl > 65534; }
+int gt_65535(signed long sl) { return sl > 65535; }
+int gt_65536(signed long sl) { return sl > 65536; }
+int gt_65537(signed long sl) { return sl > 65537; }
+int gt_131071(signed long sl) { return sl > 131071; }
+int gt_131072(signed long sl) { return sl > 131072; }
+int gt_131073(signed long sl) { return sl > 131073; }
+int gt_262143(signed long sl) { return sl > 262143; }
+int gt_262144(signed long sl) { return sl > 262144; }
+int gt_262145(signed long sl) { return sl > 262145; }
+int gt_524287(signed long sl) { return sl > 524287; }
+int gt_524288(signed long sl) { return sl > 524288; }
+int gt_524289(signed long sl) { return sl > 524289; }
+int gt_1048575(signed long sl) { return sl > 1048575; }
+int gt_1048576(signed long sl) { return sl > 1048576; }
+int gt_1048577(signed long sl) { return sl > 1048577; }
+int gt_2097151(signed long sl) { return sl > 2097151; }
+int gt_2097152(signed long sl) { return sl > 2097152; }
+int gt_2097153(signed long sl) { return sl > 2097153; }
+int gt_4194303(signed long sl) { return sl > 4194303; }
+int gt_4194304(signed long sl) { return sl > 4194304; }
+int gt_4194305(signed long sl) { return sl > 4194305; }
+int gt_8388607(signed long sl) { return sl > 8388607; }
+int gt_8388608(signed long sl) { return sl > 8388608; }
+int gt_8388609(signed long sl) { return sl > 8388609; }
+int gt_16777215(signed long sl) { return sl > 16777215; }
+int gt_16777216(signed long sl) { return sl > 16777216; }
+int gt_16777217(signed long sl) { return sl > 16777217; }
+int gt_33554431(signed long sl) { return sl > 33554431; }
+int gt_33554432(signed long sl) { return sl > 33554432; }
+int gt_33554433(signed long sl) { return sl > 33554433; }
+int gt_67108863(signed long sl) { return sl > 67108863; }
+int gt_67108864(signed long sl) { return sl > 67108864; }
+int gt_67108865(signed long sl) { return sl > 67108865; }
+int gt_134217727(signed long sl) { return sl > 134217727; }
+int gt_134217728(signed long sl) { return sl > 134217728; }
+int gt_134217729(signed long sl) { return sl > 134217729; }
+int gt_268435455(signed long sl) { return sl > 268435455; }
+int gt_268435456(signed long sl) { return sl > 268435456; }
+int gt_268435457(signed long sl) { return sl > 268435457; }
+int gt_536870911(signed long sl) { return sl > 536870911; }
+int gt_536870912(signed long sl) { return sl > 536870912; }
+int gt_536870913(signed long sl) { return sl > 536870913; }
+int gt_1073741823(signed long sl) { return sl > 1073741823; }
+int gt_1073741824(signed long sl) { return sl > 1073741824; }
+int gt_1073741825(signed long sl) { return sl > 1073741825; }
+
+static signed long sl;
+int main() {
+
+  sl = -1073741824;
+  my_assert(gt_m1073741825(sl) == 1);
+  my_assert(gt_m1073741824(sl) != 1);
+  my_assert(gt_m1073741823(sl) != 1);
+
+  sl = -536870912;
+  my_assert(gt_m536870913(sl) == 1);
+  my_assert(gt_m536870912(sl) != 1);
+  my_assert(gt_m536870911(sl) != 1);
+
+  sl = -268435456;
+  my_assert(gt_m268435457(sl) == 1);
+  my_assert(gt_m268435456(sl) != 1);
+  my_assert(gt_m268435455(sl) != 1);
+
+  sl = -134217728;
+  my_assert(gt_m134217729(sl) == 1);
+  my_assert(gt_m134217728(sl) != 1);
+  my_assert(gt_m134217727(sl) != 1);
+
+  sl = -67108864;
+  my_assert(gt_m67108865(sl) == 1);
+  my_assert(gt_m67108864(sl) != 1);
+  my_assert(gt_m67108863(sl) != 1);
+
+  sl = -33554432;
+  my_assert(gt_m33554433(sl) == 1);
+  my_assert(gt_m33554432(sl) != 1);
+  my_assert(gt_m33554431(sl) != 1);
+
+  sl = -16777216;
+  my_assert(gt_m16777217(sl) == 1);
+  my_assert(gt_m16777216(sl) != 1);
+  my_assert(gt_m16777215(sl) != 1);
+
+  sl = -8388608;
+  my_assert(gt_m8388609(sl) == 1);
+  my_assert(gt_m8388608(sl) != 1);
+  my_assert(gt_m8388607(sl) != 1);
+
+  sl = -4194304;
+  my_assert(gt_m4194305(sl) == 1);
+  my_assert(gt_m4194304(sl) != 1);
+  my_assert(gt_m4194303(sl) != 1);
+
+  sl = -2097152;
+  my_assert(gt_m2097153(sl) == 1);
+  my_assert(gt_m2097152(sl) != 1);
+  my_assert(gt_m2097151(sl) != 1);
+
+  sl = -1048576;
+  my_assert(gt_m1048577(sl) == 1);
+  my_assert(gt_m1048576(sl) != 1);
+  my_assert(gt_m1048575(sl) != 1);
+
+  sl = -524288;
+  my_assert(gt_m524289(sl) == 1);
+  my_assert(gt_m524288(sl) != 1);
+  my_assert(gt_m524287(sl) != 1);
+
+  sl = -262144;
+  my_assert(gt_m262145(sl) == 1);
+  my_assert(gt_m262144(sl) != 1);
+  my_assert(gt_m262143(sl) != 1);
+
+  sl = -131072;
+  my_assert(gt_m131073(sl) == 1);
+  my_assert(gt_m131072(sl) != 1);
+  my_assert(gt_m131071(sl) != 1);
+
+  sl = -65535;
+  my_assert(gt_m65536(sl) == 1);
+  my_assert(gt_m65535(sl) != 1);
+  my_assert(gt_m65534(sl) != 1);
+
+  sl = -32768;
+  my_assert(gt_m32769(sl) == 1);
+  my_assert(gt_m32768(sl) != 1);
+  my_assert(gt_m32767(sl) != 1);
+
+  sl = -16384;
+  my_assert(gt_m16385(sl) == 1);
+  my_assert(gt_m16384(sl) != 1);
+  my_assert(gt_m16383(sl) != 1);
+
+  sl = -8192;
+  my_assert(gt_m8193(sl) == 1);
+  my_assert(gt_m8192(sl) != 1);
+  my_assert(gt_m8191(sl) != 1);
+
+  sl = -4096;
+  my_assert(gt_m4097(sl) == 1);
+  my_assert(gt_m4096(sl) != 1);
+  my_assert(gt_m4095(sl) != 1);
+
+  sl = -2048;
+  my_assert(gt_m2049(sl) == 1);
+  my_assert(gt_m2048(sl) != 1);
+  my_assert(gt_m2047(sl) != 1);
+
+  sl = -1024;
+  my_assert(gt_m1025(sl) == 1);
+  my_assert(gt_m1024(sl) != 1);
+  my_assert(gt_m1023(sl) != 1);
+
+  sl = -512;
+  my_assert(gt_m513(sl) == 1);
+  my_assert(gt_m512(sl) != 1);
+  my_assert(gt_m511(sl) != 1);
+
+  sl = -256;
+  my_assert(gt_m257(sl) == 1);
+  my_assert(gt_m256(sl) != 1);
+  my_assert(gt_m255(sl) != 1);
+
+  sl = -128;
+  my_assert(gt_m129(sl) == 1);
+  my_assert(gt_m128(sl) != 1);
+  my_assert(gt_m127(sl) != 1);
+
+  sl = -64;
+  my_assert(gt_m65(sl) == 1);
+  my_assert(gt_m64(sl) != 1);
+  my_assert(gt_m63(sl) != 1);
+
+  sl = -32;
+  my_assert(gt_m33(sl) == 1);
+  my_assert(gt_m32(sl) != 1);
+  my_assert(gt_m31(sl) != 1);
+
+  sl = -16;
+  my_assert(gt_m17(sl) == 1);
+  my_assert(gt_m16(sl) != 1);
+  my_assert(gt_m15(sl) != 1);
+
+  sl = -8;
+  my_assert(gt_m9(sl) == 1);
+  my_assert(gt_m8(sl) != 1);
+  my_assert(gt_m7(sl) != 1);
+
+  sl = -4;
+  my_assert(gt_m5(sl) == 1);
+  my_assert(gt_m4(sl) != 1);
+  my_assert(gt_m3(sl) != 1);
+
+  sl = -2;
+  my_assert(gt_m3(sl) == 1);
+  my_assert(gt_m2(sl) != 1);
+  my_assert(gt_m1(sl) != 1);
+
+  sl = -1;
+  my_assert(gt_m2(sl) == 1);
+  my_assert(gt_m1(sl) != 1);
+  my_assert(gt_00(sl) != 1);
+
+  sl = 0;
+  my_assert(gt_m1(sl) == 1);
+  my_assert(gt_00(sl) != 1);
+  my_assert(gt_01(sl) != 1);
+
+  sl = 1;
+  my_assert(gt_00(sl) == 1);
+  my_assert(gt_01(sl) != 1);
+  my_assert(gt_02(sl) != 1);
+
+  sl = 2;
+  my_assert(gt_01(sl) == 1);
+  my_assert(gt_02(sl) != 1);
+  my_assert(gt_03(sl) != 1);
+
+  sl = 4;
+  my_assert(gt_03(sl) == 1);
+  my_assert(gt_04(sl) != 1);
+  my_assert(gt_05(sl) != 1);
+
+  sl = 8;
+  my_assert(gt_07(sl) == 1);
+  my_assert(gt_08(sl) != 1);
+  my_assert(gt_09(sl) != 1);
+
+  sl = 16;
+  my_assert(gt_15(sl) == 1);
+  my_assert(gt_16(sl) != 1);
+  my_assert(gt_17(sl) != 1);
+
+  sl = 32;
+  my_assert(gt_31(sl) == 1);
+  my_assert(gt_32(sl) != 1);
+  my_assert(gt_33(sl) != 1);
+
+  sl = 64;
+  my_assert(gt_63(sl) == 1);
+  my_assert(gt_64(sl) != 1);
+  my_assert(gt_65(sl) != 1);
+
+  sl = 128;
+  my_assert(gt_127(sl) == 1);
+  my_assert(gt_128(sl) != 1);
+  my_assert(gt_129(sl) != 1);
+
+  sl = 256;
+  my_assert(gt_255(sl) == 1);
+  my_assert(gt_256(sl) != 1);
+  my_assert(gt_257(sl) != 1);
+
+  sl = 512;
+  my_assert(gt_511(sl) == 1);
+  my_assert(gt_512(sl) != 1);
+  my_assert(gt_513(sl) != 1);
+
+  sl = 1024;
+  my_assert(gt_1023(sl) == 1);
+  my_assert(gt_1024(sl) != 1);
+  my_assert(gt_1025(sl) != 1);
+
+  sl = 2048;
+  my_assert(gt_2047(sl) == 1);
+  my_assert(gt_2048(sl) != 1);
+  my_assert(gt_2049(sl) != 1);
+
+  sl = 4096;
+  my_assert(gt_4095(sl) == 1);
+  my_assert(gt_4096(sl) != 1);
+  my_assert(gt_4097(sl) != 1);
+
+  sl = 8192;
+  my_assert(gt_8191(sl) == 1);
+  my_assert(gt_8192(sl) != 1);
+  my_assert(gt_8193(sl) != 1);
+
+  sl = 16384;
+  my_assert(gt_16383(sl) == 1);
+  my_assert(gt_16384(sl) != 1);
+  my_assert(gt_16385(sl) != 1);
+
+  sl = 32768;
+  my_assert(gt_32767(sl) == 1);
+  my_assert(gt_32768(sl) != 1);
+  my_assert(gt_32769(sl) != 1);
+
+  sl = 65536;
+  my_assert(gt_65535(sl) == 1);
+  my_assert(gt_65536(sl) != 1);
+  my_assert(gt_65537(sl) != 1);
+
+  sl = 131072;
+  my_assert(gt_131071(sl) == 1);
+  my_assert(gt_131072(sl) != 1);
+  my_assert(gt_131073(sl) != 1);
+
+  sl = 262144;
+  my_assert(gt_262143(sl) == 1);
+  my_assert(gt_262144(sl) != 1);
+  my_assert(gt_262145(sl) != 1);
+
+  sl = 524288;
+  my_assert(gt_524287(sl) == 1);
+  my_assert(gt_524288(sl) != 1);
+  my_assert(gt_524289(sl) != 1);
+
+  sl = 1048576;
+  my_assert(gt_1048575(sl) == 1);
+  my_assert(gt_1048576(sl) != 1);
+  my_assert(gt_1048577(sl) != 1);
+
+  sl = 2097152;
+  my_assert(gt_2097151(sl) == 1);
+  my_assert(gt_2097152(sl) != 1);
+  my_assert(gt_2097153(sl) != 1);
+
+  sl = 4194304;
+  my_assert(gt_4194303(sl) == 1);
+  my_assert(gt_4194304(sl) != 1);
+  my_assert(gt_4194305(sl) != 1);
+
+  sl = 8388608;
+  my_assert(gt_8388607(sl) == 1);
+  my_assert(gt_8388608(sl) != 1);
+  my_assert(gt_8388609(sl) != 1);
+
+  sl = 16777216;
+  my_assert(gt_16777215(sl) == 1);
+  my_assert(gt_16777216(sl) != 1);
+  my_assert(gt_16777217(sl) != 1);
+
+  sl = 33554432;
+  my_assert(gt_33554431(sl) == 1);
+  my_assert(gt_33554432(sl) != 1);
+  my_assert(gt_33554433(sl) != 1);
+
+  sl = 67108864;
+  my_assert(gt_67108863(sl) == 1);
+  my_assert(gt_67108864(sl) != 1);
+  my_assert(gt_67108865(sl) != 1);
+
+  sl = 134217728;
+  my_assert(gt_134217727(sl) == 1);
+  my_assert(gt_134217728(sl) != 1);
+  my_assert(gt_134217729(sl) != 1);
+
+  sl = 268435456;
+  my_assert(gt_268435455(sl) == 1);
+  my_assert(gt_268435456(sl) != 1);
+  my_assert(gt_268435457(sl) != 1);
+
+  sl = 536870912;
+  my_assert(gt_536870911(sl) == 1);
+  my_assert(gt_536870912(sl) != 1);
+  my_assert(gt_536870913(sl) != 1);
+
+  sl = 1073741824;
+  my_assert(gt_1073741823(sl) == 1);
+  my_assert(gt_1073741824(sl) != 1);
+  my_assert(gt_1073741825(sl) != 1);
+
+  return exit_status;
+}
