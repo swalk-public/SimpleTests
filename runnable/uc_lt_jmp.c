ed16d5e71b348d274539ab46a882b9c57e223487
xyzzy <xyzzy@xyzzys-iMac.local>
2025-08-03 09:22:25 -0500

    uc_lt_jmp

---
+++ runnable/uc_lt_jmp.c
@@ -0,0 +1,197 @@
+
+
+int exit_status;
+
+#include "my_assert.h"
+
+int lt_00(unsigned char uc) {
+  if (uc < 0x00)
+    return 1;
+  else
+    return 0;
+}
+int lt_01(unsigned char uc) {
+  if (uc < 0x01)
+    return 1;
+  else
+    return 0;
+}
+int lt_02(unsigned char uc) {
+  if (uc < 0x02)
+    return 1;
+  else
+    return 0;
+}
+
+int lt_03(unsigned char uc) {
+  if (uc < 0x03)
+    return 1;
+  else
+    return 0;
+}
+int lt_04(unsigned char uc) {
+  if (uc < 0x04)
+    return 1;
+  else
+    return 0;
+}
+int lt_05(unsigned char uc) {
+  if (uc < 0x05)
+    return 1;
+  else
+    return 0;
+}
+
+int lt_07(unsigned char uc) {
+  if (uc < 0x07)
+    return 1;
+  else
+    return 0;
+}
+int lt_08(unsigned char uc) {
+  if (uc < 0x08)
+    return 1;
+  else
+    return 0;
+}
+int lt_09(unsigned char uc) {
+  if (uc < 0x09)
+    return 1;
+  else
+    return 0;
+}
+
+int lt_0f(unsigned char uc) {
+  if (uc < 0x0f)
+    return 1;
+  else
+    return 0;
+}
+int lt_10(unsigned char uc) {
+  if (uc < 0x10)
+    return 1;
+  else
+    return 0;
+}
+int lt_11(unsigned char uc) {
+  if (uc < 0x11)
+    return 1;
+  else
+    return 0;
+}
+
+int lt_1f(unsigned char uc) {
+  if (uc < 0x1f)
+    return 1;
+  else
+    return 0;
+}
+int lt_20(unsigned char uc) {
+  if (uc < 0x20)
+    return 1;
+  else
+    return 0;
+}
+int lt_21(unsigned char uc) {
+  if (uc < 0x21)
+    return 1;
+  else
+    return 0;
+}
+
+int lt_3f(unsigned char uc) {
+  if (uc < 0x3f)
+    return 1;
+  else
+    return 0;
+}
+int lt_40(unsigned char uc) {
+  if (uc < 0x40)
+    return 1;
+  else
+    return 0;
+}
+int lt_41(unsigned char uc) {
+  if (uc < 0x41)
+    return 1;
+  else
+    return 0;
+}
+
+int lt_7f(unsigned char uc) {
+  if (uc < 0x7f)
+    return 1;
+  else
+    return 0;
+}
+int lt_80(unsigned char uc) {
+  if (uc < 0x80)
+    return 1;
+  else
+    return 0;
+}
+int lt_81(unsigned char uc) {
+  if (uc < 0x81)
+    return 1;
+  else
+    return 0;
+}
+
+int lt_ff(unsigned char uc) {
+  if (uc < 0xff)
+    return 1;
+  else
+    return 0;
+}
+
+static unsigned char uc;
+int main() {
+  uc = 0x00;
+  my_assert(lt_00(uc) != 1);
+  my_assert(lt_01(uc) == 1);
+
+  uc = 0x01;
+  my_assert(lt_00(uc) != 1);
+  my_assert(lt_01(uc) != 1);
+  my_assert(lt_02(uc) == 1);
+
+  uc = 0x02;
+  my_assert(lt_01(uc) != 1);
+  my_assert(lt_02(uc) != 1);
+  my_assert(lt_03(uc) == 1);
+
+  uc = 0x04;
+  my_assert(lt_03(uc) != 1);
+  my_assert(lt_04(uc) != 1);
+  my_assert(lt_05(uc) == 1);
+
+  uc = 0x08;
+  my_assert(lt_07(uc) != 1);
+  my_assert(lt_08(uc) != 1);
+  my_assert(lt_09(uc) == 1);
+
+  uc = 0x10;
+  my_assert(lt_0f(uc) != 1);
+  my_assert(lt_10(uc) != 1);
+  my_assert(lt_11(uc) == 1);
+
+  uc = 0x20;
+  my_assert(lt_1f(uc) != 1);
+  my_assert(lt_20(uc) != 1);
+  my_assert(lt_21(uc) == 1);
+
+  uc = 0x40;
+  my_assert(lt_3f(uc) != 1);
+  my_assert(lt_40(uc) != 1);
+  my_assert(lt_41(uc) == 1);
+
+  uc = 0x80;
+  my_assert(lt_7f(uc) != 1);
+  my_assert(lt_80(uc) != 1);
+  my_assert(lt_81(uc) == 1);
+
+  uc = 0xff;
+  my_assert(lt_ff(uc) != 1);
+
+  return exit_status;
+}
