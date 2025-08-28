fb95612a0cf77d66a89611f9952ee864bccdfb44
xyzzy <xyzzy@xyzzys-iMac.local>
2025-08-27 19:56:36 -0500

    ul_nge

---
+++ runnable/ul_nge.c
@@ -0,0 +1,302 @@
+
+
+int exit_status;
+#include "my_assert.h"
+
+int nge_00000000(unsigned long ul) { return !(ul >= 0x00000000ul); }
+int nge_00000001(unsigned long ul) { return !(ul >= 0x00000001ul); }
+int nge_00000002(unsigned long ul) { return !(ul >= 0x00000002ul); }
+
+int nge_00000003(unsigned long ul) { return !(ul >= 0x00000003ul); }
+int nge_00000004(unsigned long ul) { return !(ul >= 0x00000004ul); }
+int nge_00000005(unsigned long ul) { return !(ul >= 0x00000005ul); }
+
+int nge_00000007(unsigned long ul) { return !(ul >= 0x00000007ul); }
+int nge_00000008(unsigned long ul) { return !(ul >= 0x00000008ul); }
+int nge_00000009(unsigned long ul) { return !(ul >= 0x00000009ul); }
+
+int nge_0000000f(unsigned long ul) { return !(ul >= 0x0000000ful); }
+int nge_00000010(unsigned long ul) { return !(ul >= 0x00000010ul); }
+int nge_00000011(unsigned long ul) { return !(ul >= 0x00000011ul); }
+
+int nge_0000001f(unsigned long ul) { return !(ul >= 0x0000001ful); }
+int nge_00000020(unsigned long ul) { return !(ul >= 0x00000020ul); }
+int nge_00000021(unsigned long ul) { return !(ul >= 0x00000021ul); }
+
+int nge_0000003f(unsigned long ul) { return !(ul >= 0x0000003ful); }
+int nge_00000040(unsigned long ul) { return !(ul >= 0x00000040ul); }
+int nge_00000041(unsigned long ul) { return !(ul >= 0x00000041ul); }
+
+int nge_0000007f(unsigned long ul) { return !(ul >= 0x0000007ful); }
+int nge_00000080(unsigned long ul) { return !(ul >= 0x00000080ul); }
+int nge_00000081(unsigned long ul) { return !(ul >= 0x00000081ul); }
+
+int nge_000000ff(unsigned long ul) { return !(ul >= 0x000000fful); }
+int nge_00000100(unsigned long ul) { return !(ul >= 0x00000100ul); }
+int nge_00000101(unsigned long ul) { return !(ul >= 0x00000101ul); }
+
+int nge_000001ff(unsigned long ul) { return !(ul >= 0x000001fful); }
+int nge_00000200(unsigned long ul) { return !(ul >= 0x00000200ul); }
+int nge_00000201(unsigned long ul) { return !(ul >= 0x00000201ul); }
+
+int nge_000003ff(unsigned long ul) { return !(ul >= 0x000003fful); }
+int nge_00000400(unsigned long ul) { return !(ul >= 0x00000400ul); }
+int nge_00000401(unsigned long ul) { return !(ul >= 0x00000401ul); }
+
+int nge_000007ff(unsigned long ul) { return !(ul >= 0x000007fful); }
+int nge_00000800(unsigned long ul) { return !(ul >= 0x00000800ul); }
+int nge_00000801(unsigned long ul) { return !(ul >= 0x00000801ul); }
+
+int nge_00000fff(unsigned long ul) { return !(ul >= 0x00000ffful); }
+int nge_00001000(unsigned long ul) { return !(ul >= 0x00001000ul); }
+int nge_00001001(unsigned long ul) { return !(ul >= 0x00001001ul); }
+
+int nge_00001fff(unsigned long ul) { return !(ul >= 0x00001ffful); }
+int nge_00002000(unsigned long ul) { return !(ul >= 0x00002000ul); }
+int nge_00002001(unsigned long ul) { return !(ul >= 0x00002001ul); }
+
+int nge_00003fff(unsigned long ul) { return !(ul >= 0x00003ffful); }
+int nge_00004000(unsigned long ul) { return !(ul >= 0x00004000ul); }
+int nge_00004001(unsigned long ul) { return !(ul >= 0x00004001ul); }
+
+int nge_00007fff(unsigned long ul) { return !(ul >= 0x00007ffful); }
+int nge_00008000(unsigned long ul) { return !(ul >= 0x00008000ul); }
+int nge_00008001(unsigned long ul) { return !(ul >= 0x00008001ul); }
+
+int nge_0000ffff(unsigned long ul) { return !(ul >= 0x0000fffful); }
+int nge_00010000(unsigned long ul) { return !(ul >= 0x00010000ul); }
+int nge_00010001(unsigned long ul) { return !(ul >= 0x00010001ul); }
+
+int nge_0001ffff(unsigned long ul) { return !(ul >= 0x0001fffful); }
+int nge_00020000(unsigned long ul) { return !(ul >= 0x00020000ul); }
+int nge_00020001(unsigned long ul) { return !(ul >= 0x00020001ul); }
+
+int nge_0003ffff(unsigned long ul) { return !(ul >= 0x0003fffful); }
+int nge_00040000(unsigned long ul) { return !(ul >= 0x00040000ul); }
+int nge_00040001(unsigned long ul) { return !(ul >= 0x00040001ul); }
+
+int nge_0007ffff(unsigned long ul) { return !(ul >= 0x0007fffful); }
+int nge_00080000(unsigned long ul) { return !(ul >= 0x00080000ul); }
+int nge_00080001(unsigned long ul) { return !(ul >= 0x00080001ul); }
+
+int nge_000fffff(unsigned long ul) { return !(ul >= 0x000ffffful); }
+int nge_00100000(unsigned long ul) { return !(ul >= 0x00100000ul); }
+int nge_00100001(unsigned long ul) { return !(ul >= 0x00100001ul); }
+
+int nge_001fffff(unsigned long ul) { return !(ul >= 0x001ffffful); }
+int nge_00200000(unsigned long ul) { return !(ul >= 0x00200000ul); }
+int nge_00200001(unsigned long ul) { return !(ul >= 0x00200001ul); }
+
+int nge_003fffff(unsigned long ul) { return !(ul >= 0x003ffffful); }
+int nge_00400000(unsigned long ul) { return !(ul >= 0x00400000ul); }
+int nge_00400001(unsigned long ul) { return !(ul >= 0x00400001ul); }
+
+int nge_007fffff(unsigned long ul) { return !(ul >= 0x007ffffful); }
+int nge_00800000(unsigned long ul) { return !(ul >= 0x00800000ul); }
+int nge_00800001(unsigned long ul) { return !(ul >= 0x00800001ul); }
+
+int nge_00ffffff(unsigned long ul) { return !(ul >= 0x00fffffful); }
+int nge_01000000(unsigned long ul) { return !(ul >= 0x01000000ul); }
+int nge_01000001(unsigned long ul) { return !(ul >= 0x01000001ul); }
+
+int nge_01ffffff(unsigned long ul) { return !(ul >= 0x01fffffful); }
+int nge_02000000(unsigned long ul) { return !(ul >= 0x02000000ul); }
+int nge_02000001(unsigned long ul) { return !(ul >= 0x02000001ul); }
+
+int nge_03ffffff(unsigned long ul) { return !(ul >= 0x03fffffful); }
+int nge_04000000(unsigned long ul) { return !(ul >= 0x04000000ul); }
+int nge_04000001(unsigned long ul) { return !(ul >= 0x04000001ul); }
+
+int nge_07ffffff(unsigned long ul) { return !(ul >= 0x07fffffful); }
+int nge_08000000(unsigned long ul) { return !(ul >= 0x08000000ul); }
+int nge_08000001(unsigned long ul) { return !(ul >= 0x08000001ul); }
+
+int nge_0fffffff(unsigned long ul) { return !(ul >= 0x0ffffffful); }
+int nge_10000000(unsigned long ul) { return !(ul >= 0x10000000ul); }
+int nge_10000001(unsigned long ul) { return !(ul >= 0x10000001ul); }
+
+int nge_1fffffff(unsigned long ul) { return !(ul >= 0x1ffffffful); }
+int nge_20000000(unsigned long ul) { return !(ul >= 0x20000000ul); }
+int nge_20000001(unsigned long ul) { return !(ul >= 0x20000001ul); }
+
+int nge_3fffffff(unsigned long ul) { return !(ul >= 0x3ffffffful); }
+int nge_40000000(unsigned long ul) { return !(ul >= 0x40000000ul); }
+int nge_40000001(unsigned long ul) { return !(ul >= 0x40000001ul); }
+
+int nge_7fffffff(unsigned long ul) { return !(ul >= 0x7ffffffful); }
+int nge_80000000(unsigned long ul) { return !(ul >= 0x80000000ul); }
+int nge_80000001(unsigned long ul) { return !(ul >= 0x80000001ul); }
+
+int nge_ffffffff(unsigned long ul) { return !(ul >= 0xfffffffful); }
+
+static unsigned long ul;
+int main() {
+  ul = 0x00000000ul;
+  my_assert(nge_00000000(ul) != 1);
+  my_assert(nge_00000001(ul) == 1);
+
+  ul = 0x00000001ul;
+  my_assert(nge_00000000(ul) != 1);
+  my_assert(nge_00000001(ul) != 1);
+  my_assert(nge_00000002(ul) == 1);
+
+  ul = 0x00000002ul;
+  my_assert(nge_00000001(ul) != 1);
+  my_assert(nge_00000002(ul) != 1);
+  my_assert(nge_00000003(ul) == 1);
+
+  ul = 0x00000004ul;
+  my_assert(nge_00000003(ul) != 1);
+  my_assert(nge_00000004(ul) != 1);
+  my_assert(nge_00000005(ul) == 1);
+
+  ul = 0x00000008ul;
+  my_assert(nge_00000007(ul) != 1);
+  my_assert(nge_00000008(ul) != 1);
+  my_assert(nge_00000009(ul) == 1);
+
+  ul = 0x00000010ul;
+  my_assert(nge_0000000f(ul) != 1);
+  my_assert(nge_00000010(ul) != 1);
+  my_assert(nge_00000011(ul) == 1);
+
+  ul = 0x00000020ul;
+  my_assert(nge_0000001f(ul) != 1);
+  my_assert(nge_00000020(ul) != 1);
+  my_assert(nge_00000021(ul) == 1);
+
+  ul = 0x00000040ul;
+  my_assert(nge_0000003f(ul) != 1);
+  my_assert(nge_00000040(ul) != 1);
+  my_assert(nge_00000041(ul) == 1);
+
+  ul = 0x00000080ul;
+  my_assert(nge_0000007f(ul) != 1);
+  my_assert(nge_00000080(ul) != 1);
+  my_assert(nge_00000081(ul) == 1);
+
+  ul = 0x00000100ul;
+  my_assert(nge_000000ff(ul) != 1);
+  my_assert(nge_00000100(ul) != 1);
+  my_assert(nge_00000101(ul) == 1);
+
+  ul = 0x00000200ul;
+  my_assert(nge_000001ff(ul) != 1);
+  my_assert(nge_00000200(ul) != 1);
+  my_assert(nge_00000201(ul) == 1);
+
+  ul = 0x00000400ul;
+  my_assert(nge_000003ff(ul) != 1);
+  my_assert(nge_00000400(ul) != 1);
+  my_assert(nge_00000401(ul) == 1);
+
+  ul = 0x00000800ul;
+  my_assert(nge_000007ff(ul) != 1);
+  my_assert(nge_00000800(ul) != 1);
+  my_assert(nge_00000801(ul) == 1);
+
+  ul = 0x00001000ul;
+  my_assert(nge_00000fff(ul) != 1);
+  my_assert(nge_00001000(ul) != 1);
+  my_assert(nge_00001001(ul) == 1);
+
+  ul = 0x00002000ul;
+  my_assert(nge_00001fff(ul) != 1);
+  my_assert(nge_00002000(ul) != 1);
+  my_assert(nge_00002001(ul) == 1);
+
+  ul = 0x00004000ul;
+  my_assert(nge_00003fff(ul) != 1);
+  my_assert(nge_00004000(ul) != 1);
+  my_assert(nge_00004001(ul) == 1);
+
+  ul = 0x00008000ul;
+  my_assert(nge_00007fff(ul) != 1);
+  my_assert(nge_00008000(ul) != 1);
+  my_assert(nge_00008001(ul) == 1);
+
+  ul = 0x00010000ul;
+  my_assert(nge_0000ffff(ul) != 1);
+  my_assert(nge_00010000(ul) != 1);
+  my_assert(nge_00010001(ul) == 1);
+
+  ul = 0x00020000ul;
+  my_assert(nge_0001ffff(ul) != 1);
+  my_assert(nge_00020000(ul) != 1);
+  my_assert(nge_00020001(ul) == 1);
+
+  ul = 0x00040000ul;
+  my_assert(nge_0003ffff(ul) != 1);
+  my_assert(nge_00040000(ul) != 1);
+  my_assert(nge_00040001(ul) == 1);
+
+  ul = 0x00080000ul;
+  my_assert(nge_0007ffff(ul) != 1);
+  my_assert(nge_00080000(ul) != 1);
+  my_assert(nge_00080001(ul) == 1);
+
+  ul = 0x00100000ul;
+  my_assert(nge_000fffff(ul) != 1);
+  my_assert(nge_00100000(ul) != 1);
+  my_assert(nge_00100001(ul) == 1);
+
+  ul = 0x00200000ul;
+  my_assert(nge_001fffff(ul) != 1);
+  my_assert(nge_00200000(ul) != 1);
+  my_assert(nge_00200001(ul) == 1);
+
+  ul = 0x00400000ul;
+  my_assert(nge_003fffff(ul) != 1);
+  my_assert(nge_00400000(ul) != 1);
+  my_assert(nge_00400001(ul) == 1);
+
+  ul = 0x00800000ul;
+  my_assert(nge_007fffff(ul) != 1);
+  my_assert(nge_00800000(ul) != 1);
+  my_assert(nge_00800001(ul) == 1);
+
+  ul = 0x01000000ul;
+  my_assert(nge_00ffffff(ul) != 1);
+  my_assert(nge_01000000(ul) != 1);
+  my_assert(nge_01000001(ul) == 1);
+
+  ul = 0x02000000ul;
+  my_assert(nge_01ffffff(ul) != 1);
+  my_assert(nge_02000000(ul) != 1);
+  my_assert(nge_02000001(ul) == 1);
+
+  ul = 0x04000000ul;
+  my_assert(nge_03ffffff(ul) != 1);
+  my_assert(nge_04000000(ul) != 1);
+  my_assert(nge_04000001(ul) == 1);
+
+  ul = 0x08000000ul;
+  my_assert(nge_07ffffff(ul) != 1);
+  my_assert(nge_08000000(ul) != 1);
+  my_assert(nge_08000001(ul) == 1);
+
+  ul = 0x10000000ul;
+  my_assert(nge_0fffffff(ul) != 1);
+  my_assert(nge_10000000(ul) != 1);
+  my_assert(nge_10000001(ul) == 1);
+
+  ul = 0x20000000ul;
+  my_assert(nge_1fffffff(ul) != 1);
+  my_assert(nge_20000000(ul) != 1);
+  my_assert(nge_20000001(ul) == 1);
+
+  ul = 0x40000000ul;
+  my_assert(nge_3fffffff(ul) != 1);
+  my_assert(nge_40000000(ul) != 1);
+  my_assert(nge_40000001(ul) == 1);
+
+  ul = 0x80000000ul;
+  my_assert(nge_7fffffff(ul) != 1);
+  my_assert(nge_80000000(ul) != 1);
+  my_assert(nge_80000001(ul) == 1);
+
+  ul = 0xfffffffful;
+  my_assert(nge_ffffffff(ul) != 1);
+
+  return exit_status;
+}
