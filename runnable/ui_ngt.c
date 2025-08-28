f3556c5a5c79e566fc529415eb5e5dfbb3cb2143
xyzzy <xyzzy@xyzzys-iMac.local>
2025-08-27 20:06:11 -0500

    ui_ngt

---
+++ runnable/ui_ngt.c
@@ -0,0 +1,302 @@
+
+
+int exit_status;
+#include "my_assert.h"
+
+int ngt_00000000(unsigned int ui) { return !(ui > 0x00000000u); }
+int ngt_00000001(unsigned int ui) { return !(ui > 0x00000001u); }
+int ngt_00000002(unsigned int ui) { return !(ui > 0x00000002u); }
+
+int ngt_00000003(unsigned int ui) { return !(ui > 0x00000003u); }
+int ngt_00000004(unsigned int ui) { return !(ui > 0x00000004u); }
+int ngt_00000005(unsigned int ui) { return !(ui > 0x00000005u); }
+
+int ngt_00000007(unsigned int ui) { return !(ui > 0x00000007u); }
+int ngt_00000008(unsigned int ui) { return !(ui > 0x00000008u); }
+int ngt_00000009(unsigned int ui) { return !(ui > 0x00000009u); }
+
+int ngt_0000000f(unsigned int ui) { return !(ui > 0x0000000fu); }
+int ngt_00000010(unsigned int ui) { return !(ui > 0x00000010u); }
+int ngt_00000011(unsigned int ui) { return !(ui > 0x00000011u); }
+
+int ngt_0000001f(unsigned int ui) { return !(ui > 0x0000001fu); }
+int ngt_00000020(unsigned int ui) { return !(ui > 0x00000020u); }
+int ngt_00000021(unsigned int ui) { return !(ui > 0x00000021u); }
+
+int ngt_0000003f(unsigned int ui) { return !(ui > 0x0000003fu); }
+int ngt_00000040(unsigned int ui) { return !(ui > 0x00000040u); }
+int ngt_00000041(unsigned int ui) { return !(ui > 0x00000041u); }
+
+int ngt_0000007f(unsigned int ui) { return !(ui > 0x0000007fu); }
+int ngt_00000080(unsigned int ui) { return !(ui > 0x00000080u); }
+int ngt_00000081(unsigned int ui) { return !(ui > 0x00000081u); }
+
+int ngt_000000ff(unsigned int ui) { return !(ui > 0x000000ffu); }
+int ngt_00000100(unsigned int ui) { return !(ui > 0x00000100u); }
+int ngt_00000101(unsigned int ui) { return !(ui > 0x00000101u); }
+
+int ngt_000001ff(unsigned int ui) { return !(ui > 0x000001ffu); }
+int ngt_00000200(unsigned int ui) { return !(ui > 0x00000200u); }
+int ngt_00000201(unsigned int ui) { return !(ui > 0x00000201u); }
+
+int ngt_000003ff(unsigned int ui) { return !(ui > 0x000003ffu); }
+int ngt_00000400(unsigned int ui) { return !(ui > 0x00000400u); }
+int ngt_00000401(unsigned int ui) { return !(ui > 0x00000401u); }
+
+int ngt_000007ff(unsigned int ui) { return !(ui > 0x000007ffu); }
+int ngt_00000800(unsigned int ui) { return !(ui > 0x00000800u); }
+int ngt_00000801(unsigned int ui) { return !(ui > 0x00000801u); }
+
+int ngt_00000fff(unsigned int ui) { return !(ui > 0x00000fffu); }
+int ngt_00001000(unsigned int ui) { return !(ui > 0x00001000u); }
+int ngt_00001001(unsigned int ui) { return !(ui > 0x00001001u); }
+
+int ngt_00001fff(unsigned int ui) { return !(ui > 0x00001fffu); }
+int ngt_00002000(unsigned int ui) { return !(ui > 0x00002000u); }
+int ngt_00002001(unsigned int ui) { return !(ui > 0x00002001u); }
+
+int ngt_00003fff(unsigned int ui) { return !(ui > 0x00003fffu); }
+int ngt_00004000(unsigned int ui) { return !(ui > 0x00004000u); }
+int ngt_00004001(unsigned int ui) { return !(ui > 0x00004001u); }
+
+int ngt_00007fff(unsigned int ui) { return !(ui > 0x00007fffu); }
+int ngt_00008000(unsigned int ui) { return !(ui > 0x00008000u); }
+int ngt_00008001(unsigned int ui) { return !(ui > 0x00008001u); }
+
+int ngt_0000ffff(unsigned int ui) { return !(ui > 0x0000ffffu); }
+int ngt_00010000(unsigned int ui) { return !(ui > 0x00010000u); }
+int ngt_00010001(unsigned int ui) { return !(ui > 0x00010001u); }
+
+int ngt_0001ffff(unsigned int ui) { return !(ui > 0x0001ffffu); }
+int ngt_00020000(unsigned int ui) { return !(ui > 0x00020000u); }
+int ngt_00020001(unsigned int ui) { return !(ui > 0x00020001u); }
+
+int ngt_0003ffff(unsigned int ui) { return !(ui > 0x0003ffffu); }
+int ngt_00040000(unsigned int ui) { return !(ui > 0x00040000u); }
+int ngt_00040001(unsigned int ui) { return !(ui > 0x00040001u); }
+
+int ngt_0007ffff(unsigned int ui) { return !(ui > 0x0007ffffu); }
+int ngt_00080000(unsigned int ui) { return !(ui > 0x00080000u); }
+int ngt_00080001(unsigned int ui) { return !(ui > 0x00080001u); }
+
+int ngt_000fffff(unsigned int ui) { return !(ui > 0x000fffffu); }
+int ngt_00100000(unsigned int ui) { return !(ui > 0x00100000u); }
+int ngt_00100001(unsigned int ui) { return !(ui > 0x00100001u); }
+
+int ngt_001fffff(unsigned int ui) { return !(ui > 0x001fffffu); }
+int ngt_00200000(unsigned int ui) { return !(ui > 0x00200000u); }
+int ngt_00200001(unsigned int ui) { return !(ui > 0x00200001u); }
+
+int ngt_003fffff(unsigned int ui) { return !(ui > 0x003fffffu); }
+int ngt_00400000(unsigned int ui) { return !(ui > 0x00400000u); }
+int ngt_00400001(unsigned int ui) { return !(ui > 0x00400001u); }
+
+int ngt_007fffff(unsigned int ui) { return !(ui > 0x007fffffu); }
+int ngt_00800000(unsigned int ui) { return !(ui > 0x00800000u); }
+int ngt_00800001(unsigned int ui) { return !(ui > 0x00800001u); }
+
+int ngt_00ffffff(unsigned int ui) { return !(ui > 0x00ffffffu); }
+int ngt_01000000(unsigned int ui) { return !(ui > 0x01000000u); }
+int ngt_01000001(unsigned int ui) { return !(ui > 0x01000001u); }
+
+int ngt_01ffffff(unsigned int ui) { return !(ui > 0x01ffffffu); }
+int ngt_02000000(unsigned int ui) { return !(ui > 0x02000000u); }
+int ngt_02000001(unsigned int ui) { return !(ui > 0x02000001u); }
+
+int ngt_03ffffff(unsigned int ui) { return !(ui > 0x03ffffffu); }
+int ngt_04000000(unsigned int ui) { return !(ui > 0x04000000u); }
+int ngt_04000001(unsigned int ui) { return !(ui > 0x04000001u); }
+
+int ngt_07ffffff(unsigned int ui) { return !(ui > 0x07ffffffu); }
+int ngt_08000000(unsigned int ui) { return !(ui > 0x08000000u); }
+int ngt_08000001(unsigned int ui) { return !(ui > 0x08000001u); }
+
+int ngt_0fffffff(unsigned int ui) { return !(ui > 0x0fffffffu); }
+int ngt_10000000(unsigned int ui) { return !(ui > 0x10000000u); }
+int ngt_10000001(unsigned int ui) { return !(ui > 0x10000001u); }
+
+int ngt_1fffffff(unsigned int ui) { return !(ui > 0x1fffffffu); }
+int ngt_20000000(unsigned int ui) { return !(ui > 0x20000000u); }
+int ngt_20000001(unsigned int ui) { return !(ui > 0x20000001u); }
+
+int ngt_3fffffff(unsigned int ui) { return !(ui > 0x3fffffffu); }
+int ngt_40000000(unsigned int ui) { return !(ui > 0x40000000u); }
+int ngt_40000001(unsigned int ui) { return !(ui > 0x40000001u); }
+
+int ngt_7fffffff(unsigned int ui) { return !(ui > 0x7fffffffu); }
+int ngt_80000000(unsigned int ui) { return !(ui > 0x80000000u); }
+int ngt_80000001(unsigned int ui) { return !(ui > 0x80000001u); }
+
+int ngt_ffffffff(unsigned int ui) { return !(ui > 0xffffffffu); }
+
+static unsigned int ui;
+int main() {
+  ui = 0x00000000u;
+  my_assert(ngt_00000000(ui) == 1);
+  my_assert(ngt_00000001(ui) == 1);
+
+  ui = 0x00000001u;
+  my_assert(ngt_00000000(ui) != 1);
+  my_assert(ngt_00000001(ui) == 1);
+  my_assert(ngt_00000002(ui) == 1);
+
+  ui = 0x00000002u;
+  my_assert(ngt_00000001(ui) != 1);
+  my_assert(ngt_00000002(ui) == 1);
+  my_assert(ngt_00000003(ui) == 1);
+
+  ui = 0x00000004u;
+  my_assert(ngt_00000003(ui) != 1);
+  my_assert(ngt_00000004(ui) == 1);
+  my_assert(ngt_00000005(ui) == 1);
+
+  ui = 0x00000008u;
+  my_assert(ngt_00000007(ui) != 1);
+  my_assert(ngt_00000008(ui) == 1);
+  my_assert(ngt_00000009(ui) == 1);
+
+  ui = 0x00000010u;
+  my_assert(ngt_0000000f(ui) != 1);
+  my_assert(ngt_00000010(ui) == 1);
+  my_assert(ngt_00000011(ui) == 1);
+
+  ui = 0x00000020u;
+  my_assert(ngt_0000001f(ui) != 1);
+  my_assert(ngt_00000020(ui) == 1);
+  my_assert(ngt_00000021(ui) == 1);
+
+  ui = 0x00000040u;
+  my_assert(ngt_0000003f(ui) != 1);
+  my_assert(ngt_00000040(ui) == 1);
+  my_assert(ngt_00000041(ui) == 1);
+
+  ui = 0x00000080u;
+  my_assert(ngt_0000007f(ui) != 1);
+  my_assert(ngt_00000080(ui) == 1);
+  my_assert(ngt_00000081(ui) == 1);
+
+  ui = 0x00000100u;
+  my_assert(ngt_000000ff(ui) != 1);
+  my_assert(ngt_00000100(ui) == 1);
+  my_assert(ngt_00000101(ui) == 1);
+
+  ui = 0x00000200u;
+  my_assert(ngt_000001ff(ui) != 1);
+  my_assert(ngt_00000200(ui) == 1);
+  my_assert(ngt_00000201(ui) == 1);
+
+  ui = 0x00000400u;
+  my_assert(ngt_000003ff(ui) != 1);
+  my_assert(ngt_00000400(ui) == 1);
+  my_assert(ngt_00000401(ui) == 1);
+
+  ui = 0x00000800u;
+  my_assert(ngt_000007ff(ui) != 1);
+  my_assert(ngt_00000800(ui) == 1);
+  my_assert(ngt_00000801(ui) == 1);
+
+  ui = 0x00001000u;
+  my_assert(ngt_00000fff(ui) != 1);
+  my_assert(ngt_00001000(ui) == 1);
+  my_assert(ngt_00001001(ui) == 1);
+
+  ui = 0x00002000u;
+  my_assert(ngt_00001fff(ui) != 1);
+  my_assert(ngt_00002000(ui) == 1);
+  my_assert(ngt_00002001(ui) == 1);
+
+  ui = 0x00004000u;
+  my_assert(ngt_00003fff(ui) != 1);
+  my_assert(ngt_00004000(ui) == 1);
+  my_assert(ngt_00004001(ui) == 1);
+
+  ui = 0x00008000u;
+  my_assert(ngt_00007fff(ui) != 1);
+  my_assert(ngt_00008000(ui) == 1);
+  my_assert(ngt_00008001(ui) == 1);
+
+  ui = 0x00010000u;
+  my_assert(ngt_0000ffff(ui) != 1);
+  my_assert(ngt_00010000(ui) == 1);
+  my_assert(ngt_00010001(ui) == 1);
+
+  ui = 0x00020000u;
+  my_assert(ngt_0001ffff(ui) != 1);
+  my_assert(ngt_00020000(ui) == 1);
+  my_assert(ngt_00020001(ui) == 1);
+
+  ui = 0x00040000u;
+  my_assert(ngt_0003ffff(ui) != 1);
+  my_assert(ngt_00040000(ui) == 1);
+  my_assert(ngt_00040001(ui) == 1);
+
+  ui = 0x00080000u;
+  my_assert(ngt_0007ffff(ui) != 1);
+  my_assert(ngt_00080000(ui) == 1);
+  my_assert(ngt_00080001(ui) == 1);
+
+  ui = 0x00100000u;
+  my_assert(ngt_000fffff(ui) != 1);
+  my_assert(ngt_00100000(ui) == 1);
+  my_assert(ngt_00100001(ui) == 1);
+
+  ui = 0x00200000u;
+  my_assert(ngt_001fffff(ui) != 1);
+  my_assert(ngt_00200000(ui) == 1);
+  my_assert(ngt_00200001(ui) == 1);
+
+  ui = 0x00400000u;
+  my_assert(ngt_003fffff(ui) != 1);
+  my_assert(ngt_00400000(ui) == 1);
+  my_assert(ngt_00400001(ui) == 1);
+
+  ui = 0x00800000u;
+  my_assert(ngt_007fffff(ui) != 1);
+  my_assert(ngt_00800000(ui) == 1);
+  my_assert(ngt_00800001(ui) == 1);
+
+  ui = 0x01000000u;
+  my_assert(ngt_00ffffff(ui) != 1);
+  my_assert(ngt_01000000(ui) == 1);
+  my_assert(ngt_01000001(ui) == 1);
+
+  ui = 0x02000000u;
+  my_assert(ngt_01ffffff(ui) != 1);
+  my_assert(ngt_02000000(ui) == 1);
+  my_assert(ngt_02000001(ui) == 1);
+
+  ui = 0x04000000u;
+  my_assert(ngt_03ffffff(ui) != 1);
+  my_assert(ngt_04000000(ui) == 1);
+  my_assert(ngt_04000001(ui) == 1);
+
+  ui = 0x08000000u;
+  my_assert(ngt_07ffffff(ui) != 1);
+  my_assert(ngt_08000000(ui) == 1);
+  my_assert(ngt_08000001(ui) == 1);
+
+  ui = 0x10000000u;
+  my_assert(ngt_0fffffff(ui) != 1);
+  my_assert(ngt_10000000(ui) == 1);
+  my_assert(ngt_10000001(ui) == 1);
+
+  ui = 0x20000000u;
+  my_assert(ngt_1fffffff(ui) != 1);
+  my_assert(ngt_20000000(ui) == 1);
+  my_assert(ngt_20000001(ui) == 1);
+
+  ui = 0x40000000u;
+  my_assert(ngt_3fffffff(ui) != 1);
+  my_assert(ngt_40000000(ui) == 1);
+  my_assert(ngt_40000001(ui) == 1);
+
+  ui = 0x80000000u;
+  my_assert(ngt_7fffffff(ui) != 1);
+  my_assert(ngt_80000000(ui) == 1);
+  my_assert(ngt_80000001(ui) == 1);
+
+  ui = 0xffffffffu;
+  my_assert(ngt_ffffffff(ui) == 1);
+
+  return exit_status;
+}
